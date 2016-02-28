fa826857f424015a5332c2dd3b1199f11711bc11
mnegretev <mnegretev@gmail.com>
2016-02-27 21:57:04 -0600

    First compilation in La-Mas-Negra

--- catkin_ws/build/catkin_generated/order_packages.py
+++ catkin_ws/build/catkin_generated/order_packages.py
@@ -2,4 +2,4 @@
 source_root_dir = "/home/marco/JUSTINA/catkin_ws/src"
 whitelisted_packages = "".split(';') if "" != "" else []
 blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/home/marco/catkin_ws/devel;/opt/ros/indigo".split(';') if "/home/marco/catkin_ws/devel;/opt/ros/indigo" != "" else []
+underlay_workspaces = "/opt/ros/indigo".split(';') if "/opt/ros/indigo" != "" else []
