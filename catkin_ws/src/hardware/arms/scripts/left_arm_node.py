daab2b08b71e80ac04f10570b17aedac3ecd7743
edgarVazquez43 <edgar@justina.com>
2016-03-09 12:49:05 -0600

    first attemp to conversion byte----- rad for Dinamixel Servos

--- catkin_ws/src/hardware/arms/scripts/left_arm_node.py
+++ catkin_ws/src/hardware/arms/scripts/left_arm_node.py
@@ -20,9 +20,35 @@ def main(portName1, portBaud1, portName2, portBaud2):
     dynMan2 = Dynamixel.DynamixelMan(portName2, portBaud2)
     while not rospy.is_shutdown():
         pos0 = dynMan1.GetPresentPosition(0)
+        pos0 = ((2094-pos0)/(4095))*(3.141592)
+
         pos1 = dynMan1.GetPresentPosition(1)
+        pos1 = ((3127-pos1)/(4095))*(3.141592)
+        
         pos2 = dynMan1.GetPresentPosition(2)
-        print "Poses: " + str(pos0) + "   " + str(pos1) + "   " + str(pos2)
+        pos2 = ((1798-pos2)/(4095))*(3.141592)
+
+        pos3 = dynMan1.GetPresentPosition(3)
+        pos3 = ((1997-pos3)/(4095))*(3.141592)
+
+        pos4 = dynMan1.GetPresentPosition(4)
+        pos4 = ((2050-pos4)/(4095))*(3.141592)
+        
+        pos5 = dynMan1.GetPresentPosition(5)
+        pos5 = ((1774-pos5)/(4095))*(3.141592)
+        
+        pos6 = dynMan1.GetPresentPosition(6)
+        pos6 = ((2048-pos6)/(4095)*(3.141592)
+        
+        pos7 = dynMan2.GetPresentPosition(7)
+        pos7 = ((512-pos7)/(1023))*(3.141592)
+        
+        pos8 = dynMan2.GetPresentPosition(107)
+        pos8 = ((512-pos8)/(1023))*(3.141592)
+        
+        print "Poses: " + str(pos0) + "  " + str(pos1) + "  " + str(pos2) + "  " + str(pos3)
+             + "  " + str(pos4) + "  " + str(pos5) + "  " + str(pos6) + "  " + str(pos7) + "  " + str(pos8)
+             
         loop.sleep()
 
 if __name__ == '__main__':
