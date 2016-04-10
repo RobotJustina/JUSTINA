4f042d1168bad0f64d92b795666fdc886cb537a0
Marco Negrete <mnegretev@uni-koblenz.de>
2016-04-09 22:42:01 -0500

    First version of low_level_control for manipulators

---
+++ catkin_ws/src/manipulation/low_level_moves/src/InverseKinematics.cpp
@@ -0,0 +1,13 @@
+#include "InverseKinematics.h"
+
+bool InverseKinematics::GetInverseKinematics(std::vector<float>& cartesian, std::vector<float>& articular)
+{
+}
+
+bool InverseKinematics::GetInverseKinematics(geometry_msgs::Pose& cartesian, std::vector<float>& articular)
+{
+}
+
+bool InverseKinematics::GetInverseKinematics(nav_msgs::Path& cartesianPath, std::vector<std_msgs::Float32MultiArray> articularPath)
+{
+}
