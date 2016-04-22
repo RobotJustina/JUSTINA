89f94bd15d83e3103c4cefda8c0a969ded715586
edgarVazquez43 <edgar@justina.com>
2016-04-22 16:02:52 -0500

    Add the b_button of joystick for stop the mobile base

--- catkin_ws/src/interoperation/joystick_teleop/scripts/joystick_teleop_node.py
+++ catkin_ws/src/interoperation/joystick_teleop/scripts/joystick_teleop_node.py
@@ -3,12 +3,17 @@ import math
 import rospy
 from sensor_msgs.msg import Joy
 from std_msgs.msg import Float32MultiArray
+from std_msgs.msg import Empty
 
 def callbackJoy(msg):
     global leftSpeed
     global rightSpeed
     global panPos
     global tiltPos
+    global b_Button
+
+    ### Read of b_button for stop the mobile base
+    b_Button = msg.buttons[1]
 
     ### Control of head with left Stick 
     leftStickX = msg.axes[0]
@@ -40,6 +45,7 @@ def main():
     global rightSpeed
     global panPos 
     global tiltPos
+    global b_Button
     
     print "INITIALIZING JOYSTICK TELEOP BY MARCOSOFT..."
     rospy.init_node("joystick_teleop")
@@ -48,6 +54,7 @@ def main():
     rospy.Subscriber("/hardware/joy", Joy, callbackJoy)
     pubSpeeds = rospy.Publisher("/hardware/mobile_base/speeds", Float32MultiArray, queue_size=1)
     pubHeadPos = rospy.Publisher("/hardware/head/goal_pose", Float32MultiArray, queue_size=1)
+    pubStop = rospy.Publisher("/hardware/robot_state/stop", Empty, queue_size=1)
     #pubHeadTorque = rospy.Publisher("/hardware/head/torque", Float32MultiArray, queue_size=1)
  
 
@@ -59,8 +66,10 @@ def main():
     rightSpeed = 0
     panPos = 0
     tiltPos = 0
+    b_Button = 0
     msgSpeeds = Float32MultiArray()
     msgHeadPos = Float32MultiArray()
+    msgStop = Empty()
     #msgHeadTorque = Float32MultiArray()
 
     while not rospy.is_shutdown():
@@ -72,6 +81,9 @@ def main():
         if math.fabs(panPos) > 0 or math.fabs(tiltPos) > 0:
             headPos.data = [panPos, tiltPos]
             pubHeadPos.publish(headPos)
+
+        if b_Button == 1:
+            pubStop.publish(msgStop)
 
         #if math.fabs(panPos) > 0 or math.fabs(tiltPos) > 0:
             #msgHeadTorque.data = [panPos, tiltPos]
