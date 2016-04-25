55586c21e9bffb93d02004582951fe3330a0580f
mnegretev <mnegretev@uni-koblenz.de>
2016-04-25 12:45:48 -0500

    Change justina_gui for using a QtProject

---
+++ catkin_ws/src/hri/justina_gui/src/justina_gui/justina_gui.pro
@@ -0,0 +1,20 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2016-04-24T20:27:06
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = justina_gui
+TEMPLATE = app
+
+
+SOURCES += justina_gui_node.cpp\
+        MainWindow.cpp
+
+HEADERS  += MainWindow.h
+
+FORMS    += MainWindow.ui
--- catkin_ws/src/hri/justina_gui/src/justina_gui/justina_gui_node.cpp
+++ catkin_ws/src/hri/justina_gui/src/justina_gui/justina_gui_node.cpp
@@ -1,23 +1,25 @@
 #include <iostream>
+#include <QApplication>
 #include "justina_tools/JustinaHardware.h"
 #include "MainWindow.h"
+#include "QtRosNode.h"
 
 int main(int argc, char** argv)
 {
     std::cout << "INITIALIZING JUSTINA GUI BY MARCOSOFT" << std::endl;
     ros::init(argc, argv, "justina_gui");
     ros::NodeHandle n;
-    
+
     QtRosNode qtRosNode;
     qtRosNode.setNodeHandle(&n);
     qtRosNode.start();
-    
+
     QApplication app(argc, argv);
     MainWindow mainWindow;
-    mainWindow.setWindowTitle(QString::fromUtf8("JUSTINA GUI BY MARCOSOFT"));
-    mainWindow.resize(640, 480);
-    mainWindow.setRosNode(&qtRosNode);
-    
+    //mainWindow.setWindowTitle(QString::fromUtf8("JUSTINA GUI BY MARCOSOFT"));
+    //mainWindow.resize(640, 480);
+    //mainWindow.setRosNode(&qtRosNode);
+
     mainWindow.show();
     return app.exec();
 }
