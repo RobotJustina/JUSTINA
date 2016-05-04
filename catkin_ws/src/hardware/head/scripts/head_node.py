9fd8e72b7ad9aaf2e850b299726f7a9b7b981c29
Marco Negrete <mnegretev@uni-koblenz.de>
2016-05-03 23:42:26 -0500

    Add read battery to head node

--- catkin_ws/src/hardware/head/scripts/head_node.py
+++ catkin_ws/src/hardware/head/scripts/head_node.py
@@ -109,6 +109,7 @@ def main(portName, portBaud):
     dynMan1 = Dynamixel.DynamixelMan(portName, portBaud)
     pan = 0;
     tilt = 0;
+    i = 0
 
     ### Set controller parameters
     dynMan1.SetDGain(1, 25)
@@ -138,6 +139,7 @@ def main(portName, portBaud):
     subPosition = rospy.Subscriber("/hardware/head/goal_pose", Float32MultiArray, callbackPosHead)
     #subTorque = rospy.Subscriber("/torque", Float32MultiArray, callbackTorque)
     pubJointStates = rospy.Publisher("/joint_states", JointState, queue_size = 1)
+    pubBatery = rospy.Publisher("/hardware/robot_state/head_battery", Float32, queue_size = 1)
     
     bitsPerRadian = (1023)/((300)*(3.14159265358979323846/180))
 
@@ -172,6 +174,13 @@ def main(portName, portBaud):
         jointStates.position[0] = pan
         jointStates.position[1] = -tilt #A tilt > 0 goes upwards, but to keep a dextereous system, positive tilt should go downwards
         pubJointStates.publish(jointStates)
+
+        if i == 10:
+            msgBatery = float(dynMan1.GetPresentVoltage(5)/10)
+            pubBatery.publish(msgBatery)
+            i=0
+        i+=1
+        
         loop.sleep()
 
 if __name__ == '__main__':
