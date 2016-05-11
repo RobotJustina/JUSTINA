741bd7e10c109bd432a8033a16f3d1b0de278d4f
mnegretev <mnegretev@uni-koblenz.de>
2016-05-11 15:53:05 -0500

    Update to mvn_pln

--- catkin_ws/src/navigation/mvn_pln/src/MvnPln.cpp
+++ catkin_ws/src/navigation/mvn_pln/src/MvnPln.cpp
@@ -8,6 +8,18 @@ MvnPln::~MvnPln()
 {
 }
 
+void initROSConnection(ros::NodeHandle* nh)
+{
+    this->nh = nh;
+    this->subGetCloseLoc = nh->subscribe("/navigation/mvn_pln/get_close_loc", 1, &MvnPln::callbackGetCloseLoc, this);
+    this->subGetCloseXYA = nh->subscribe("/navigation/mvn_pln/get_close_xya", 1, &MvnPln::callbackGetCloseXYA, this);
+    this->pubGoalReached = nh->advertise<std_msgs::Bool>("/navigation/goal_reached", 1);
+}
+
+void spin()
+{
+}
+
 bool MvnPln::GetClose(std::string location)
 {
 }
@@ -18,5 +30,13 @@ bool MvnPln::GetClose(float goalX, float goalY)
 }
 
 bool MvnPln::GetClose(float goalX, float goalY, float goalTheta)
+{
+}
+
+void MvnPln::callbackGetCloseLoc(const std_msgs::String::ConstPtr& msg)
+{
+}
+
+void MvnPln::callbackGetCloseXYA(const std_msgs::Float32MultiArray::ConstPtr& msg)
 {
 }
--- catkin_ws/src/navigation/mvn_pln/src/mvn_pln_node.cpp
+++ catkin_ws/src/navigation/mvn_pln/src/mvn_pln_node.cpp
@@ -18,9 +18,7 @@ int main(int argc, char** argv)
     std::cout << "INITIALIZING MOVING PLANNER BY MARCOSOFT..." << std::endl;
     ros::init(argc, argv, "mvn_pln");
     ros::NodeHandle n;
-    ros::Subscriber subGetCloseLoc = n.subscribe("/navigation/mvn_pln/get_close_loc", 1, callbackGetCloseLoc);
-    ros::Subscriber subGetCloseXYA = n.subscribe("/navigation/mvn_pln/get_close_xya", 1, callbackGetCloseXYA);
-    ros::Publisher pubGoalReached = n.advertise<std_msgs::Bool>("/navigation/goal_reached", 1);
+    
     ros::Rate loop(10);
 
     JustinaNavigation::setNodeHandle(&n);
