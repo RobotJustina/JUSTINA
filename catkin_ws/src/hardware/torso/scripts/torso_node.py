b6b67fc185dab753896991e22c40aeefdd08f585
edgarVazquez43 <edgar@justina.com>
2016-05-11 22:50:40 -0500

    Add subscriber to goal position into torso_node

--- catkin_ws/src/hardware/torso/scripts/torso_node.py
+++ catkin_ws/src/hardware/torso/scripts/torso_node.py
@@ -7,6 +7,10 @@ from geometry_msgs.msg import TransformStamped
 from sensor_msgs.msg import JointState
 import tf
 
+def callbackGoalPos(msg):
+    global goalPos
+    goalPos = msg.data 
+
 
 def main():
     print "INITIALIZING TORSO NODE..."
@@ -14,17 +18,22 @@ def main():
     ###Connection with ROS
     rospy.init_node("head")
     br = tf.TransformBroadcaster()
+    msgCurrentPos = Float32()
     jointStates = JointState()
     jointStates.name = ["shoulders_connect"]
-    jointStates.position = [0]
+    jointStates.position = [0.0]
     
     ## Subscribers
     pubJointStates = rospy.Publisher("/joint_states", JointState, queue_size = 1)
+    pubCurrentPos = rospy.Publisher("/hardware/torso_current_pose", Float32, queue_size=1)
+    subGoalPos = rospy.Subscriber("/hardware/torso_goal_pose", Float32, callbackGoalPos)
+
    
     loop = rospy.Rate(10)
     
     while not rospy.is_shutdown():
-       
+        jointStates.position = [goalPos]
+        pubCurrentPos.publish(pos)
         pubJointStates.publish(jointStates)
 
         loop.sleep()
