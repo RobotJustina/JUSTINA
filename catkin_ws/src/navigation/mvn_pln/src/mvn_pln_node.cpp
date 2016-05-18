e0695c33cc2685e3d1993076477010b65a335ee6
Marco Negrete <mnegretev@uni-koblenz.de>
2016-05-18 00:00:33 -0500

    Add loadKnownLocations to mvn_pln

--- catkin_ws/src/navigation/mvn_pln/src/mvn_pln_node.cpp
+++ catkin_ws/src/navigation/mvn_pln/src/mvn_pln_node.cpp
@@ -4,25 +4,17 @@
 #include "std_msgs/String.h"
 #include "std_msgs/Float32MultiArray.h"
 #include "justina_tools/JustinaNavigation.h"
-
-void callbackGetCloseLoc(const std_msgs::String::ConstPtr& msg)
-{
-}
-
-void callbackGetCloseXYA(const std_msgs::Float32MultiArray::ConstPtr& msg)
-{
-    
-}
+#include "MvnPln.h"
 
 int main(int argc, char** argv)
 {
     std::cout << "INITIALIZING MOVING PLANNER BY MARCOSOFT..." << std::endl;
     ros::init(argc, argv, "mvn_pln");
     ros::NodeHandle n;
-    
     ros::Rate loop(10);
-
+    
     JustinaNavigation::setNodeHandle(&n);
+    MvnPln mvnPln;
 
     while(ros::ok())
     {
