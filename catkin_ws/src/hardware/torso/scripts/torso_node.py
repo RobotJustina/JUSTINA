92e0195efd8611a840032967fe0660434ceb9496
Edgar <ing_edd@outlook.com>
2016-06-07 21:02:07 -0500

    Changes in the torso_node: spine, waist, shouldres

--- catkin_ws/src/hardware/torso/scripts/torso_node.py
+++ catkin_ws/src/hardware/torso/scripts/torso_node.py
@@ -7,38 +7,60 @@ from geometry_msgs.msg import TransformStamped
 from sensor_msgs.msg import JointState
 import tf
 
-def callbackGoalPos(msg):
-    global goalPos
-    goalPos = msg.data 
+def callbackGoalSpine(msg):
+    global goalSpine
+    goalSpine = msg.data 
+
+def callbackGoalWaist(msg):
+    global goalWaist
+    goalWaist = msg.data
+
+def callbackGoalShoulders(msg):
+    global goalShoulders
+    goalShoulders = msg.data
 
 
 def main():
     print "INITIALIZING TORSO NODE..."
-    global goalPos
-    goalPos = 0.0
-    
+    global goalSpine
+    global goalWaist
+    global goalShouldes
+    goalSpine = 0.0
+    goalWaist = 0.0
+    goalShoulders = 0.0
+       
     ###Connection with ROS
     rospy.init_node("torso")
     br = tf.TransformBroadcaster()
-    msgCurrentPos = Float32()
+    msgCurrentSpin = Float32()
+    msgCurrentWaist = Float32()
+    msgCurrentShoulders = Float32()
+	
     jointStates = JointState()
-    jointStates.name = ["torso_connect"]
-    jointStates.position = [0.0]
+    jointStates.name = ["spine_connect","waist_connect","shoulders_connect"]
+    jointStates.position = [0.0,0.0,0.0]
     
     ## Subscribers
     pubJointStates = rospy.Publisher("/joint_states", JointState, queue_size = 1)
-    pubCurrentPos = rospy.Publisher("/hardware/torso/current_pose", Float32, queue_size=1)
-    subGoalPos = rospy.Subscriber("/hardware/torso/goal_pose", Float32, callbackGoalPos)
+    pubCurrentSpine = rospy.Publisher("/hardware/torso/current_spine", Float32, queue_size=1)
+    pubCurrentWaist = rospy.Publisher("/hardware/torso/current_waist", Float32, queue_size=1)
+    pubCurrentShoulders = rospy.Publisher("/hardware/torso/current_shoulders", Float32, queue_size=1)
 
-   
+    subGoalPos = rospy.Subscriber("/hardware/torso/goal_spine", Float32, callbackGoalSpine)
+    subGoalPos = rospy.Subscriber("/hardware/torso/goal_waist", Float32, callbackGoalWaist)
+    subGoalPos = rospy.Subscriber("/hardware/torso/goal_shoulders", Float32, callbackGoalShoulders)
+
     loop = rospy.Rate(10)
     
     while not rospy.is_shutdown():
         jointStates.header.stamp = rospy.Time.now()
-        jointStates.position = [goalPos]
-        pubCurrentPos.publish(msgCurrentPos)
+        jointStates.position = [goalSpine,goalWaist,goalShoulders]
+    
+        pubCurrentSpine.publish(msgCurrentSpin)
+	pubCurrentWaist.publish(msgCurrentWaist)
+	pubCurrentShoulders.publish(msgCurrentShoulders)
+	
         pubJointStates.publish(jointStates)
-
         loop.sleep()
 
 if __name__ == '__main__':
