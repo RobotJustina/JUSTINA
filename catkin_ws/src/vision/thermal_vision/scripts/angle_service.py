90f5e597fb21c0d63a3cefefb5e0ac9c1007480f
Kigs-mx <karlos.kigs@gmail.com>
2016-06-23 17:51:48 -0500

    angle service function

--- catkin_ws/src/vision/thermal_vision/scripts/angle_service.py
+++ catkin_ws/src/vision/thermal_vision/scripts/angle_service.py
@@ -15,7 +15,7 @@ from vision_msgs.srv import GetThermalAngleResponse
 min_th = 140
 max_th = 255
 
-min_area = 1000
+min_area = 5000
 max_area = 50000
 
 img_center = [165,128]
@@ -82,8 +82,9 @@ def human_scaner(image):
         
         #print "angulo en grados"
         #print math.degrees(angle_p)
-    #else:
-        #print ":("
+    else:
+        angle_p = 10.0
+        return angle_p
 
 def callback_2(data):
     global angle_f
