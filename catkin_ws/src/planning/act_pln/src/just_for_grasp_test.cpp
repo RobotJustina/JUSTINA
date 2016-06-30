d28a1c9bbf2bc2b2eaaee763dd4f4ef34074b0a8
Mark The Magnificent <mnegretev@gmail.com>
2016-07-01 00:07:54 +0200

    change the name of the grap object

--- catkin_ws/src/planning/act_pln/src/just_for_grasp_test.cpp
+++ catkin_ws/src/planning/act_pln/src/just_for_grasp_test.cpp
@@ -18,7 +18,7 @@ int main(int argc, char** argv)
     std::vector<vision_msgs::VisionObject> recognizedObjects;
     bool found;
     int indexFound = 0;
-    std::string idObject = "coconut-milk";
+    std::string idObject = "pringles";
 
     while(ros::ok() && !fail && !success){
         switch(nextState){
