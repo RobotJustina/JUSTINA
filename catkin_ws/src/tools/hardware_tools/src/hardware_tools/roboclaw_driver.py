4ac716689183ad605129eec5efa7d2e5cd5d9ba8
edgarVazquez43 <edgar_vazquez43@hotmail.com>
2016-12-18 19:46:09 -0600

    Corrections of some errors into printing lib's hardwareTools

--- catkin_ws/src/tools/hardware_tools/src/hardware_tools/roboclaw_driver.py
+++ catkin_ws/src/tools/hardware_tools/src/hardware_tools/roboclaw_driver.py
@@ -1183,6 +1183,7 @@ def ReadPWMMode(address):
 def Open(comport, rate):
     global port
     port = serial.Serial(comport, baudrate=rate, timeout=0.1, interCharTimeout=0.01)
+    print "HardwareTools-Roboclaw.-> Open port: " + str(comport) + " at: " + str(rate)     
     return
 
 def Close():
