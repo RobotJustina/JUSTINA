a523480199208b29eb3db79c297d56bf748bd6dc
edgarVazquez43 <edgar_vazquez43@hotmail.com>
2017-01-19 18:49:21 -0600

    Add package obj_angle_calc

---
+++ catkin_ws/src/vision/obj_angle_calc/src/obj_angle_calc_node.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/opencv.hpp>
+#include "ros/ros.h"
+
+#include "justina_tools/JustinaTools.h"
+
+#include "plane3D.hpp"
+#include "findPlaneRansac.hpp"
+
+
+
+int main(int argc, char** argv)
+{
+
+    return 0;
+}
