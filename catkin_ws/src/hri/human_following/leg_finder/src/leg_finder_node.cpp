3a71f25b3757298d36c681d86bb03c12f3012319
EdgarSilva77 <ing_edd@outlook.com>
2017-03-25 14:23:36 -0600

    add a topic that publishes a boolean message on the node leg_finder

--- catkin_ws/src/hri/human_following/leg_finder/src/leg_finder_node.cpp
+++ catkin_ws/src/hri/human_following/leg_finder/src/leg_finder_node.cpp
@@ -19,6 +19,8 @@ std::vector<float> laser_angles;
 bool laserUpdate = false;
 bool enable = false;
 bool frontalLegsFound = false;
+int legsFound_c=0;
+int legsLoses_c=0;
 
 void callbackLaserScan(const sensor_msgs::LaserScan::ConstPtr& msg)
 {
@@ -52,14 +54,14 @@ int main(int argc, char** argv)
     ros::Subscriber subLaserScan = n.subscribe("/hardware/scan", 1, callbackLaserScan);
     ros::Subscriber subEnableDetection = n.subscribe("/hri/leg_finder/enable", 1, callbackEnable);
     ros::Publisher pubLegPose = n.advertise<geometry_msgs::PointStamped>("/hri/leg_finder/leg_poses", 1);
-    ros::Publisher pubLegsFound = n.advertise<std_msgs::Empty>("/hri/leg_finder/legs_found", 1);
+    ros::Publisher pubLegsFound = n.advertise<std_msgs::Bool>("/hri/leg_finder/legs_found", 1);
     tf::TransformListener tf_listener;
         
     LegFinder legs = LegFinder();
     pcl::PointXYZ legPos;
     float distan;
     geometry_msgs::PointStamped msgLegs;
-    std_msgs::Empty msgLegsFound;
+    std_msgs::Bool msgLegsFound;
 
     ros::Rate loop(10);
     msgLegs.header.frame_id = "base_link";
@@ -74,7 +76,12 @@ int main(int argc, char** argv)
                 legs.laserCallback(laser_ranges, laser_angles);
                 legs.findPiernasFrente(legPos, 2.0, 0.3);
                 if(frontalLegsFound = legs.isThereMotionlessLegInFront())
-                    pubLegsFound.publish(msgLegsFound);
+                    msgLegsFound.data=true;
+                
+                else
+                    msgLegsFound.data=false; 
+                
+                pubLegsFound.publish(msgLegsFound);
             }
             else
             {
@@ -83,6 +90,22 @@ int main(int argc, char** argv)
                 //std::cout<<legsPos.x<<" "<<legsPos.y<<std::endl;
                 msgLegs.point.x=legPos.x;
                 msgLegs.point.y=legPos.y;
+
+                if(legPos.x == 0 && legPos.y == 0){
+                    legsLoses_c++;
+                    legsFound_c=0;
+                    if(legsLoses_c > 7)
+                        msgLegsFound.data=false;
+                       
+                }
+                else{
+                    legsFound_c++;
+                    legsLoses_c=0;
+                    if(legsFound_c > 7)
+                        msgLegsFound.data=true;
+                }
+
+                pubLegsFound.publish(msgLegsFound);
                 pubLegPose.publish(msgLegs);
             }
         }
