9d04a8c45ccd8f67adefb1907aa9f8ff2d4d971e
Mark The Magnificent <mnegretev@gmail.com>
2017-03-30 19:13:17 +0200

    The real know location

--- catkin_ws/src/tests/func_test/src/grasp_object_test.cpp
+++ catkin_ws/src/tests/func_test/src/grasp_object_test.cpp
@@ -19,7 +19,7 @@ int main(int argc, char** argv)
     std::vector<vision_msgs::VisionObject> recognizedObjects;
     bool found;
     int indexFound = 0;
-    std::string idObject = "milk";
+    std::string idObject = "soup";
     bool withLeftOrRightArm;
 
     while(ros::ok() && !fail && !success){
