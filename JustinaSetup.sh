8efd794192c73e8e6b24646a31113b7139291aab
rmartella <reymar_44@hotmail.com>
2017-05-11 13:35:53 -0500

    Add the new Justina setup to branch of the Update ros

--- JustinaSetup.sh
+++ JustinaSetup.sh
@@ -1,47 +1,42 @@
-echo "INSTALLING  DEPENDENCIES..."
-sudo apt get update
+sudo apt-get update
 sudo apt-get install freeglut3-dev pkg-config build-essential libxmu-dev libxi-dev libusb-1.0-0-dev doxygen graphviz mono-complete
-sudo apt-get install build-essential libgtk2.0-dev libjpeg-dev libtiff4-dev libjasper-dev libopenexr-dev cmake python-dev python-numpy python-tk libtbb-dev libeigen3-dev yasm libfaac-dev libopencore-amrnb-dev libopencore-amrwb-dev libtheora-dev libvorbis-dev libxvidcore-dev libx264-dev libqt4-dev libqt4-opengl-dev sphinx-common texlive-latex-extra libv4l-dev libdc1394-22-dev libavcodec-dev libavformat-dev libswscale-dev default-jdk ant libvtk5-qt4-dev
-cd ~
-echo "INSTALLING OPENCV 2.4.9"
-wget http://sourceforge.net/projects/opencvlibrary/files/opencv-unix/2.4.9/opencv-2.4.9.zip
-unzip opencv-2.4.9.zip
-cd opencv-2.4.9
+sudo apt-get install build-essential libgtk2.0-dev libjpeg-dev libtiff5-dev libjasper-dev libopenexr-dev cmake python-dev python-numpy python-tk libtbb-dev libeigen3-dev yasm libfaac-dev libopencore-amrnb-dev libopencore-amrwb-dev libtheora-dev libvorbis-dev libxvidcore-dev libx264-dev sphinx-common texlive-latex-extra libv4l-dev libdc1394-22-dev libavcodec-dev libavformat-dev libswscale-dev ant
+#Se eliminan estas librerias por la actualización del QT
+#libqt4-dev libqt4-opengl-dev libvtk5-qt4-dev default-jdk
+wget https://sourceforge.net/projects/opencvlibrary/files/opencv-unix/3.2.0/opencv-3.2.0.zip
+wget https://github.com/opencv/opencv_contrib/archive/3.2.0.zip
+mv 3.2.0.zip opencv_contrib-3.2.0.zip
+unzip opencv-3.2.0.zip
+unzip opencv_contrib-3.2.0.zip
+cd opencv-3.2.0
 mkdir build
 cd build
-cmake -D WITH_TBB=ON -D BUILD_NEW_PYTHON_SUPPORT=ON -D WITH_V4L=ON -D INSTALL_C_EXAMPLES=ON -D INSTALL_PYTHON_EXAMPLES=ON -D BUILD_EXAMPLES=ON -D WITH_QT=ON -D WITH_OPENGL=ON -D WITH_VTK=ON -D WITH_OPENNI=ON -D WITH_OPENCL=OFF ..
+cmake -D WITH_TBB=ON -D BUILD_NEW_PYTHON_SUPPORT=ON -D WITH_V4L=ON -D INSTALL_C_EXAMPLES=ON -D INSTALL_PYTHON_EXAMPLES=ON -D BUILD_EXAMPLES=ON -D WITH_QT=ON -D WITH_OPENGL=ON -D WITH_VTK=ON -D WITH_OPENNI=ON -D WITH_OPENCL=OFF -DOPENCV_EXTRA_MODULES_PATH=../../opencv_contrib-3.2.0/modules ..
 make -j4
 sudo make install
 sudo echo "/usr/local/lib" >> /etc/ld.so.conf.d/opencv.conf
 sudo ldconfig
-echo "OpenCV is ready to use"
-echo "Installing PrimeSense drivers..."
 mkdir -p prime_sense
 cd prime_sense
 git clone https://github.com/ph4m/SensorKinect.git
+cd SensorKinect
 git checkout unstable
-cd SensorKinect/Platform/Linux/CreateRedist
+cd ../SensorKinect/Platform/Linux/CreateRedist
 ./RedistMaker
 cd ../Redist/Sensor-Bin-Linux-x64-v5.1.2.1/
 sudo ./install.sh
 echo "Prime sense drivers correctly installed"
-echo "INSTALLING POINT CLOUDS LIBRARY..."
-sudo add-apt-repository ppa:v-launchpad-jochen-sprickerhof-de/pcl
-sudo apt-get update
-sudo apt-get install libpcl-all
-echo "Installing OTHER NEEDED ROS PACKAGES..."
-sudo apt-get install ros-indigo-hokuyo-node
-sudo apt-get install ros-indigo-joy
-sudo apt-get install ros-indigo-openni-camera
-sudo apt-get install ros-indigo-openni-launch
-sudo apt-get install ros-indigo-openni2-camera
-sudo apt-get install ros-indigo-openni2-launch
-sudo apt-get install ros-indigo-amcl
-sudo apt-get install ros-indigo-tf2-bullet
-sudo apt-get install ros-indigo-fake-localization
-sudo apt-get install ros-indigo-map-server
-sudo apt-get install ros-indigo-sound-play
-sudo apt-get install ros-indigo-pocketsphinx
+sudo apt-get install ros-kinetic-urg-node
+sudo apt-get install ros-kinetic-joy
+sudo apt-get install ros-kinetic-openni-camera
+sudo apt-get install ros-kinetic-openni-launch
+sudo apt-get install ros-kinetic-openni2-camera
+sudo apt-get install ros-kinetic-openni2-launch
+sudo apt-get install ros-kinetic-amcl
+sudo apt-get install ros-kinetic-tf2-bullet
+sudo apt-get install ros-kinetic-fake-localization
+sudo apt-get install ros-kinetic-map-server
+sudo apt-get install ros-kinetic-sound-play
 echo "INSTALLING OTHER NEEDED LIBRARIES..."
 sudo apt-get install libzbar-dev
 sudo apt-get purge jackd2 jackd jackd2-firewire libjack-jackd2-dev pulseaudio-module-jack qjackctl
