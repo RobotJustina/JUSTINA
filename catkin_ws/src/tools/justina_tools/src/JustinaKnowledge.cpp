69f8e99b6a17133e671f355b24c2d5b3ac3d2eda
rmartella <reymar_44@hotmail.com>
2017-05-19 12:20:07 -0500

    Correct the bug when close the gui

--- catkin_ws/src/tools/justina_tools/src/JustinaKnowledge.cpp
+++ catkin_ws/src/tools/justina_tools/src/JustinaKnowledge.cpp
@@ -10,6 +10,7 @@
 ros::ServiceClient * JustinaKnowledge::cliKnownLoc;
 ros::ServiceClient * JustinaKnowledge::cliAddUpKnownLoc;
 ros::Subscriber * JustinaKnowledge::subUpdateKnowmLoc;
+ros::Subscriber * JustinaKnowledge::subInitKnowmLoc;
 ros::Publisher * JustinaKnowledge::pubEnableEdit;
 ros::Publisher * JustinaKnowledge::pubLoadFromFile;
 ros::Publisher * JustinaKnowledge::pubDeleteKnownLoc;
@@ -18,233 +19,246 @@ ros::ServiceClient * JustinaKnowledge::cliGetPredQues;
 ros::ServiceClient * JustinaKnowledge::cliGetPredLaArmPose;
 ros::ServiceClient * JustinaKnowledge::cliGetPredRaArmPose;
 bool JustinaKnowledge::updateKnownLoc = false;
+bool JustinaKnowledge::initKnownLoc = true;
 tf::TransformListener* JustinaKnowledge::tf_listener;
 
 JustinaKnowledge::~JustinaKnowledge(){
-  delete cliKnownLoc;
-  delete cliAddUpKnownLoc;
-  delete subUpdateKnowmLoc;
-  delete pubEnableEdit;
-  delete pubLoadFromFile;
-  delete pubDeleteKnownLoc;
-  delete pubSaveInFile;
-  delete cliGetPredLaArmPose;
-  delete cliGetPredRaArmPose;
-  delete tf_listener;
+    delete cliKnownLoc;
+    delete cliAddUpKnownLoc;
+    delete subUpdateKnowmLoc;
+    delete subInitKnowmLoc;
+    delete pubEnableEdit;
+    delete pubLoadFromFile;
+    delete pubDeleteKnownLoc;
+    delete pubSaveInFile;
+    delete cliGetPredLaArmPose;
+    delete cliGetPredRaArmPose;
+    delete tf_listener;
 }
 
 void JustinaKnowledge::setNodeHandle(ros::NodeHandle * nh) {
-  tf_listener = new tf::TransformListener();
-	cliKnownLoc = new ros::ServiceClient(
-      nh->serviceClient<knowledge_msgs::KnownLocations>(
-					"/knowledge/known_locations"));
-  cliAddUpKnownLoc = new ros::ServiceClient(
-      nh->serviceClient<knowledge_msgs::AddUpdateKnownLoc>(
-          "/knowledge/add_update_known_locations"));
-  subUpdateKnowmLoc = new ros::Subscriber(
-      nh->subscribe("/knowledge/update_location_markers", 1, &JustinaKnowledge::callBackUpdateKnownLoc));
-  pubEnableEdit = new ros::Publisher(
-      nh->advertise<std_msgs::Bool>("/knowledge/edit_known_loc", 1));
-  pubLoadFromFile = new ros::Publisher(
-      nh->advertise<std_msgs::String>("/knowledge/load_from_file", 1));
-  pubDeleteKnownLoc = new ros::Publisher(
-      nh->advertise<std_msgs::String>("/knowledge/delete_known_locations", 1));
-  pubSaveInFile = new ros::Publisher(
-      nh->advertise<std_msgs::String>("/knowledge/save_in_file", 1));
-  cliGetPredQues = new ros::ServiceClient(
-      nh->serviceClient<knowledge_msgs::GetPredefinedQuestions>(
-          "/knowledge/get_predefined_questions"));
-  cliGetPredLaArmPose = new ros::ServiceClient(
-	nh->serviceClient<knowledge_msgs::GetPredefinedArmsPoses>(
-		"/knowledge/la_predefined_poses"));
-  cliGetPredRaArmPose = new ros::ServiceClient(
-	nh->serviceClient<knowledge_msgs::GetPredefinedArmsPoses>(
-		"/knowledge/ra_predefined_poses"));
-  tf_listener->waitForTransform("map", "base_link", ros::Time(0), ros::Duration(5.0));
+    tf_listener = new tf::TransformListener();
+    cliKnownLoc = new ros::ServiceClient(
+            nh->serviceClient<knowledge_msgs::KnownLocations>(
+                "/knowledge/known_locations"));
+    cliAddUpKnownLoc = new ros::ServiceClient(
+            nh->serviceClient<knowledge_msgs::AddUpdateKnownLoc>(
+                "/knowledge/add_update_known_locations"));
+    subUpdateKnowmLoc = new ros::Subscriber(
+            nh->subscribe("/knowledge/update_location_markers", 1, &JustinaKnowledge::callBackUpdateKnownLoc));
+    subInitKnowmLoc = new ros::Subscriber(
+            nh->subscribe("/knowledge/init_location_markers", 1, &JustinaKnowledge::callBackInitKnownLoc));
+    pubEnableEdit = new ros::Publisher(
+            nh->advertise<std_msgs::Bool>("/knowledge/edit_known_loc", 1));
+    pubLoadFromFile = new ros::Publisher(
+            nh->advertise<std_msgs::String>("/knowledge/load_from_file", 1));
+    pubDeleteKnownLoc = new ros::Publisher(
+            nh->advertise<std_msgs::String>("/knowledge/delete_known_locations", 1));
+    pubSaveInFile = new ros::Publisher(
+            nh->advertise<std_msgs::String>("/knowledge/save_in_file", 1));
+    cliGetPredQues = new ros::ServiceClient(
+            nh->serviceClient<knowledge_msgs::GetPredefinedQuestions>(
+                "/knowledge/get_predefined_questions"));
+    cliGetPredLaArmPose = new ros::ServiceClient(
+            nh->serviceClient<knowledge_msgs::GetPredefinedArmsPoses>(
+                "/knowledge/la_predefined_poses"));
+    cliGetPredRaArmPose = new ros::ServiceClient(
+            nh->serviceClient<knowledge_msgs::GetPredefinedArmsPoses>(
+                "/knowledge/ra_predefined_poses"));
+    tf_listener->waitForTransform("map", "base_link", ros::Time(0), ros::Duration(5.0));
 }
 
 void JustinaKnowledge::callBackUpdateKnownLoc(
-    const std_msgs::Bool::ConstPtr updateKnownLoc){
-  JustinaKnowledge::updateKnownLoc = updateKnownLoc->data;
+        const std_msgs::Bool::ConstPtr updateKnownLoc){
+    JustinaKnowledge::updateKnownLoc = updateKnownLoc->data;
+}
+
+void JustinaKnowledge::callBackInitKnownLoc(
+        const std_msgs::Bool::ConstPtr initKnownLoc){
+    JustinaKnowledge::initKnownLoc = initKnownLoc->data;
 }
 
 void JustinaKnowledge::getRobotPose(float &currentX, float &currentY, float &currentTheta){
-  tf::StampedTransform transform;
-  tf::Quaternion q;
-  tf_listener->lookupTransform("map", "base_link", ros::Time(0), transform);
-  q = transform.getRotation();
+    tf::StampedTransform transform;
+    tf::Quaternion q;
+    tf_listener->lookupTransform("map", "base_link", ros::Time(0), transform);
+    q = transform.getRotation();
 
-  currentX = transform.getOrigin().x();
-  currentY = transform.getOrigin().y();
-  currentTheta = atan2((float)q.z(), (float)q.w()) * 2;
+    currentX = transform.getOrigin().x();
+    currentY = transform.getOrigin().y();
+    currentTheta = atan2((float)q.z(), (float)q.w()) * 2;
 }
 
 void JustinaKnowledge::getKnownLocations(
-		std::map<std::string, std::vector<float> >& locations) {
-  knowledge_msgs::KnownLocations srv;
-  if (cliKnownLoc->call(srv)) {
-    for (std::vector<knowledge_msgs::MapKnownLocation>::iterator it =
-				srv.response.locations.begin();
-        it != srv.response.locations.end(); ++it) {
-			locations.insert(
-					std::pair<std::string, std::vector<float> >(it->name,
-							it->value));
-		}
-	} else {
-		ROS_ERROR("Failed to call service known_locations");
-	}
+        std::map<std::string, std::vector<float> >& locations) {
+    knowledge_msgs::KnownLocations srv;
+    if (cliKnownLoc->call(srv)) {
+        for (std::vector<knowledge_msgs::MapKnownLocation>::iterator it =
+                srv.response.locations.begin();
+                it != srv.response.locations.end(); ++it) {
+            locations.insert(
+                    std::pair<std::string, std::vector<float> >(it->name,
+                        it->value));
+        }
+    } else {
+        ROS_ERROR("Failed to call service known_locations");
+    }
 }
 
 void JustinaKnowledge::getUpdateKnownLoc(bool& updateKnownLoc){
-  updateKnownLoc = JustinaKnowledge::updateKnownLoc;
+    updateKnownLoc = JustinaKnowledge::updateKnownLoc;
+}
+
+void JustinaKnowledge::getInitKnownLoc(bool& initKnownLoc){
+    initKnownLoc = JustinaKnowledge::initKnownLoc;
 }
 
 void JustinaKnowledge::enableInteractiveUpdate(bool enable){
-  std_msgs::Bool msg;
-  msg.data = enable;
-  pubEnableEdit->publish(msg);
+    std_msgs::Bool msg;
+    msg.data = enable;
+    pubEnableEdit->publish(msg);
 }
 
 void JustinaKnowledge::loadFromFile(const std::string filePath){
-  std_msgs::String msg;
-  msg.data = filePath;
-  pubLoadFromFile->publish(msg);
+    std_msgs::String msg;
+    msg.data = filePath;
+    pubLoadFromFile->publish(msg);
 }
 
 void JustinaKnowledge::saveInFile(const std::string filePath){
-  std_msgs::String msg;
-  msg.data = filePath;
-  pubSaveInFile->publish(msg);
+    std_msgs::String msg;
+    msg.data = filePath;
+    pubSaveInFile->publish(msg);
 }
 
 void JustinaKnowledge::addUpdateKnownLoc(std::string name, std::vector<float> values){
-  knowledge_msgs::AddUpdateKnownLoc srv;
-  srv.request.loc.name = name;
-  srv.request.loc.value = values;
-  if (cliAddUpKnownLoc->call(srv)) {
-  } else {
-    ROS_ERROR("Failed to call service known_locations");
-  }
+    knowledge_msgs::AddUpdateKnownLoc srv;
+    srv.request.loc.name = name;
+    srv.request.loc.value = values;
+    if (cliAddUpKnownLoc->call(srv)) {
+    } else {
+        ROS_ERROR("Failed to call service known_locations");
+    }
 }
 
 void JustinaKnowledge::addUpdateKnownLoc(std::string name){
-  knowledge_msgs::AddUpdateKnownLoc srv;
-	std::vector<float> values;
-	float x, y, theta;
-  getRobotPose(x, y, theta);
-	srv.request.loc.name = name;
-	values.push_back(x);
-	values.push_back(y);
-	srv.request.loc.value = values;
-	if (cliAddUpKnownLoc->call(srv)) {
-	} else {
-		ROS_ERROR("Failed to call service known_locations");
-	}
+    knowledge_msgs::AddUpdateKnownLoc srv;
+    std::vector<float> values;
+    float x, y, theta;
+    getRobotPose(x, y, theta);
+    srv.request.loc.name = name;
+    values.push_back(x);
+    values.push_back(y);
+    srv.request.loc.value = values;
+    if (cliAddUpKnownLoc->call(srv)) {
+    } else {
+        ROS_ERROR("Failed to call service known_locations");
+    }
 }
 
 void JustinaKnowledge::addUpdateKnownLoc(std::string name, float ori){
-  knowledge_msgs::AddUpdateKnownLoc srv;
-	std::vector<float> values;
-	float x, y, theta;
-  getRobotPose(x, y, theta);
-	values.push_back(x);
-	values.push_back(y);
-	values.push_back(ori);
-	srv.request.loc.name = name;
-	srv.request.loc.value = values;
-	if (cliAddUpKnownLoc->call(srv)) {
-	} else {
-		ROS_ERROR("Failed to call service known_locations");
-	}
+    knowledge_msgs::AddUpdateKnownLoc srv;
+    std::vector<float> values;
+    float x, y, theta;
+    getRobotPose(x, y, theta);
+    values.push_back(x);
+    values.push_back(y);
+    values.push_back(ori);
+    srv.request.loc.name = name;
+    srv.request.loc.value = values;
+    if (cliAddUpKnownLoc->call(srv)) {
+    } else {
+        ROS_ERROR("Failed to call service known_locations");
+    }
 }
 
 void JustinaKnowledge::addUpdateKnownLoc(std::string name, float x, float y){
-  knowledge_msgs::AddUpdateKnownLoc srv;
-	std::vector<float> values;
-	values.push_back(x);
-	values.push_back(y);
-	srv.request.loc.name = name;
-	srv.request.loc.value = values;
-	if (cliAddUpKnownLoc->call(srv)) {
-	} else {
-		ROS_ERROR("Failed to call service known_locations");
-	}
+    knowledge_msgs::AddUpdateKnownLoc srv;
+    std::vector<float> values;
+    values.push_back(x);
+    values.push_back(y);
+    srv.request.loc.name = name;
+    srv.request.loc.value = values;
+    if (cliAddUpKnownLoc->call(srv)) {
+    } else {
+        ROS_ERROR("Failed to call service known_locations");
+    }
 }
 
 void JustinaKnowledge::addUpdateKnownLoc(std::string name, float x, float y, float ori){
-  knowledge_msgs::AddUpdateKnownLoc srv;
-	std::vector<float> values;
-	values.push_back(x);
-	values.push_back(y);
-	values.push_back(ori);
-	srv.request.loc.name = name;
-	srv.request.loc.value = values;
-	if (cliAddUpKnownLoc->call(srv)) {
-	} else {
-		ROS_ERROR("Failed to call service known_locations");
-	}
+    knowledge_msgs::AddUpdateKnownLoc srv;
+    std::vector<float> values;
+    values.push_back(x);
+    values.push_back(y);
+    values.push_back(ori);
+    srv.request.loc.name = name;
+    srv.request.loc.value = values;
+    if (cliAddUpKnownLoc->call(srv)) {
+    } else {
+        ROS_ERROR("Failed to call service known_locations");
+    }
 }
 
 void JustinaKnowledge::deleteKnownLoc(const std::string name){
-  std_msgs::String msg;
-  msg.data = name;
-  pubDeleteKnownLoc->publish(msg);
+    std_msgs::String msg;
+    msg.data = name;
+    pubDeleteKnownLoc->publish(msg);
 }
 
 void JustinaKnowledge::getPredQuestions(std::map<std::string, std::string> &predQues){
-  knowledge_msgs::GetPredefinedQuestions srv;
-  if (cliGetPredQues->call(srv)) {
-     for(int i = 0; i < srv.response.predefinedQuestions.size(); i++){
-       predQues[srv.response.predefinedQuestions[i].question] = srv.response.predefinedQuestions[i].answer;
-     }
-  } else {
-    ROS_ERROR("Failed to call service known_locations");
-  }
+    knowledge_msgs::GetPredefinedQuestions srv;
+    if (cliGetPredQues->call(srv)) {
+        for(int i = 0; i < srv.response.predefinedQuestions.size(); i++){
+            predQues[srv.response.predefinedQuestions[i].question] = srv.response.predefinedQuestions[i].answer;
+        }
+    } else {
+        ROS_ERROR("Failed to call service known_locations");
+    }
 }
- 
+
 void JustinaKnowledge::getPredQuestions(std::vector<std::string> &questions){
-	knowledge_msgs::GetPredefinedQuestions srv;
-	if (cliGetPredQues->call(srv)) {
-     		for(int i = 0; i < srv.response.predefinedQuestions.size(); i++){
-       			questions.push_back(srv.response.predefinedQuestions[i].question);
-     		}
-  	} else {
-    		ROS_ERROR("Failed to call service known_locations");
-	}
+    knowledge_msgs::GetPredefinedQuestions srv;
+    if (cliGetPredQues->call(srv)) {
+        for(int i = 0; i < srv.response.predefinedQuestions.size(); i++){
+            questions.push_back(srv.response.predefinedQuestions[i].question);
+        }
+    } else {
+        ROS_ERROR("Failed to call service known_locations");
+    }
 }
 
 bool JustinaKnowledge::comparePredQuestion(std::string question, std::string &answer){
-  std::map<std::string, std::string> predQues;
-  getPredQuestions(predQues);
-  boost::replace_all(question, ",", " ");
-  boost::replace_all(question, ".", " ");
-  std::cout << "JustinaKnowledge.->Ask answer:" << question << std::endl;
-  /*std::replace(question.begin(), question.end(), "," , " ");
-  std::replace(question.begin(), question.end(), "," , ".");*/
-  std::map<std::string, std::string>::iterator quesFound = predQues.find(question);
-  if(quesFound == predQues.end())
-    return false;
-  answer = quesFound->second;
-  std::cout << "JustinaKnowledge.->Answer:" << answer << std::endl;
-  return true;
+    std::map<std::string, std::string> predQues;
+    getPredQuestions(predQues);
+    boost::replace_all(question, ",", " ");
+    boost::replace_all(question, ".", " ");
+    std::cout << "JustinaKnowledge.->Ask answer:" << question << std::endl;
+    /*std::replace(question.begin(), question.end(), "," , " ");
+      std::replace(question.begin(), question.end(), "," , ".");*/
+    std::map<std::string, std::string>::iterator quesFound = predQues.find(question);
+    if(quesFound == predQues.end())
+        return false;
+    answer = quesFound->second;
+    std::cout << "JustinaKnowledge.->Answer:" << answer << std::endl;
+    return true;
 }
 
 void JustinaKnowledge::getPredLaArmPose(std::string name, std::vector<float> &poses){
-	knowledge_msgs::GetPredefinedArmsPoses srv;
-	srv.request.name = name;
-	if (cliGetPredLaArmPose->call(srv)) {
-		for(int i = 0; i < srv.response.angles.size(); i++)
-			poses.push_back(srv.response.angles[i].data);
-  	} else 
-    		ROS_ERROR("Failed to call service known_locations");
+    knowledge_msgs::GetPredefinedArmsPoses srv;
+    srv.request.name = name;
+    if (cliGetPredLaArmPose->call(srv)) {
+        for(int i = 0; i < srv.response.angles.size(); i++)
+            poses.push_back(srv.response.angles[i].data);
+    } else 
+        ROS_ERROR("Failed to call service known_locations");
 }
 
 void JustinaKnowledge::getPredRaArmPose(std::string name, std::vector<float> &poses){
-	knowledge_msgs::GetPredefinedArmsPoses srv;
-	srv.request.name = name;
-	if (cliGetPredRaArmPose->call(srv)) {
-		for(int i = 0; i < srv.response.angles.size(); i++)
-			poses.push_back(srv.response.angles[i].data);
-  	} else 
-    		ROS_ERROR("Failed to call service known_locations");
+    knowledge_msgs::GetPredefinedArmsPoses srv;
+    srv.request.name = name;
+    if (cliGetPredRaArmPose->call(srv)) {
+        for(int i = 0; i < srv.response.angles.size(); i++)
+            poses.push_back(srv.response.angles[i].data);
+    } else 
+        ROS_ERROR("Failed to call service known_locations");
 }
