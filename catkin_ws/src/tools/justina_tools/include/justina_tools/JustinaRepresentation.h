1e6eb3a9c7d7ed095ce8bf7fda4e9f9f46cf8054
rmartella <reymar_44@hotmail.com>
2017-06-02 22:36:14 -0500

    Add the service fot the interpretation and querys

--- catkin_ws/src/tools/justina_tools/include/justina_tools/JustinaRepresentation.h
+++ catkin_ws/src/tools/justina_tools/include/justina_tools/JustinaRepresentation.h
@@ -24,35 +24,38 @@
 #include <boost/algorithm/string/classification.hpp>
 
 class JustinaRepresentation {
-private:
-	ros::NodeHandle * nh;
-	
-  static ros::Publisher * command_runCLIPS;
-  static ros::Publisher * command_resetCLIPS;
-  static ros::Publisher * command_factCLIPS;
-  static ros::Publisher * command_ruleCLIPS;
-  static ros::Publisher * command_agendaCLIPS;
-  static ros::Publisher * command_sendCLIPS;
-  static ros::Publisher * command_loadCLIPS;
-  
+    private:
+        ros::NodeHandle * nh;
 
-public:
+        static ros::Publisher * command_runCLIPS;
+        static ros::Publisher * command_resetCLIPS;
+        static ros::Publisher * command_factCLIPS;
+        static ros::Publisher * command_ruleCLIPS;
+        static ros::Publisher * command_agendaCLIPS;
+        static ros::Publisher * command_sendCLIPS;
+        static ros::Publisher * command_loadCLIPS;
+        static ros::ServiceClient * cliSpechInterpretation;
+        static ros::ServiceClient * cliStringInterpretation;
 
-  ~JustinaRepresentation();
+    public:
 
-	static void setNodeHandle(ros::NodeHandle * nh);
-	static void runCLIPS(bool enable);
-	static void resetCLIPS(bool enable);
-    static void factCLIPS(bool enable);
-    static void ruleCLIPS(bool enable);
-	static void agendaCLIPS(bool enable);
-	static void sendCLIPS(std::string command);
-    static void loadCLIPS(std::string file);
-    static void getLocations(std::string path ,std::map<std::string, std::vector<std::string> >& locations);
-    static void getObjects(std::string path ,std::map<std::string, std::vector<std::string> >& objects);
-    static void addLocations(std::map<std::string, std::vector<std::string> >& locations, std::string name, std::vector<std::string> values);
-    static void addObjects(std::map<std::string, std::vector<std::string> >& objects, std::string name, std::vector<std::string> values);
-  
+        ~JustinaRepresentation();
+
+        static void setNodeHandle(ros::NodeHandle * nh);
+        static void runCLIPS(bool enable);
+        static void resetCLIPS(bool enable);
+        static void factCLIPS(bool enable);
+        static void ruleCLIPS(bool enable);
+        static void agendaCLIPS(bool enable);
+        static void sendCLIPS(std::string command);
+        static void loadCLIPS(std::string file);
+        static void getLocations(std::string path ,std::map<std::string, std::vector<std::string> >& locations);
+        static void getObjects(std::string path ,std::map<std::string, std::vector<std::string> >& objects);
+        static void addLocations(std::map<std::string, std::vector<std::string> >& locations, std::string name, std::vector<std::string> values);
+        static void addObjects(std::map<std::string, std::vector<std::string> >& objects, std::string name, std::vector<std::string> values);
+        static bool speachInterpretation();
+        static bool stringInterpretation(std::string stringToInterpretation);
+
 };
 
 #endif /* TOOLS_JUSTINA_TOOLS_SRC_JUSTINAREPRESENTATION_H_ */
