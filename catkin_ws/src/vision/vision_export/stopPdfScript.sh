0f60c47b206f578a2448f2ef7ff50c36de2b8d30
Hector Rodrigo Arce Gonzalez <rag_@live.com>
2017-06-07 19:22:19 -0500

    Added USB auto-copy
    Remember to run " JustinaSetup.sh" in order to create "/media/usbPDF" directory

--- catkin_ws/src/vision/vision_export/stopPdfScript.sh
+++ catkin_ws/src/vision/vision_export/stopPdfScript.sh
@@ -13,6 +13,15 @@ FILE=/home/$USER/JUSTINA/catkin_ws/src/vision/vision_export/temp/$TEST.tex;
 mkdir $OPATH;
 echo "\\end{document}" >> $FILE;
 pdflatex -output-directory=$OPATH $FILE;
+REMOVABLE_DEVICE=$(find /sys/block/ -type l -printf "%f %l\n" | awk '/usb/{print $1}')
+
+if grep -Eq "(sdb|sdc)" <<< "$REMOVABLE_DEVICE" ;then
+	sudo mount /dev/sdb /media/usbPDF
+	sudo mount /dev/sdc /media/usbPDF
+	cp $OPATH/$FILE /media/usbPDF
+else
+	echo "USB NOT PRESENT!, ONLY LOCAL PDF CREATED"
+fi
 rm /home/$USER/JUSTINA/catkin_ws/src/vision/vision_export/temp/$1.tex;
 rm /home/$USER/JUSTINA/catkin_ws/src/vision/vision_export/temp/*.log;
 rm /home/$USER/JUSTINA/catkin_ws/src/vision/vision_export/temp/*.aux;
