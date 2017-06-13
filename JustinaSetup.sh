9dd77ee98137e5546996e65acc23a02839d4f5ca
rag21 <rag_@live.com>
2017-06-13 14:15:49 -0500

    Improved messages from installation script JustinaSetup.sh

--- JustinaSetup.sh
+++ JustinaSetup.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
+#FORMAT
+FRM='\033['
+BLACK='0;30'
+RED='1;31'
+GREEN='1;32'
+YELLOW='1;33'
+BLUE='1;34'
+PURPLE='1;35'
+CYAN='1;36'
+WHITE='1;37'
+BGRED=';41m'
+BGGREEN=';42m'
+BGYELLOW=';43m'
+BGBLUE=';44m'
+BGWHITE=';47m'
+NC='\033[0m'
+#SCRIPT START
 sudo apt-get update
 sudo apt-get install -y freeglut3-dev pkg-config build-essential libxmu-dev libxi-dev libusb-1.0-0-dev doxygen graphviz mono-complete
 sudo apt-get install -y build-essential libgtk2.0-dev libjpeg-dev libtiff5-dev libjasper-dev libopenexr-dev cmake python-dev python-numpy python-tk libtbb-dev libeigen3-dev yasm libfaac-dev libopencore-amrnb-dev libopencore-amrwb-dev libtheora-dev libvorbis-dev libxvidcore-dev libx264-dev sphinx-common texlive-latex-extra libv4l-dev libdc1394-22-dev libavcodec-dev libavformat-dev libswscale-dev ant default-jdk
@@ -22,7 +39,8 @@ cd build
 cmake -D WITH_TBB=ON -D BUILD_NEW_PYTHON_SUPPORT=ON -D WITH_V4L=ON -D INSTALL_C_EXAMPLES=ON -D INSTALL_PYTHON_EXAMPLES=ON -D BUILD_EXAMPLES=ON -D WITH_QT=ON -D WITH_OPENGL=ON -D WITH_VTK=ON -D WITH_OPENNI=ON -D WITH_OPENCL=OFF -DOPENCV_EXTRA_MODULES_PATH=../../opencv_contrib-3.2.0/modules ..
 make -j4
 sudo make install
-sudo echo "/usr/local/lib" >> /etc/ld.so.conf.d/opencv.conf
+sudo touch /etc/ld.so.conf.d/opencv.conf
+sudo /bin/su -c "echo '/usr/local/lib' >> /etc/ld.so.conf.d/opencv.conf"
 sudo ldconfig
 cd $HOME
 mkdir -p prime_sense
@@ -37,8 +55,8 @@ cd ../SensorKinect/Platform/Linux/CreateRedist
 ./RedistMaker
 cd ../Redist/Sensor-Bin-Linux-x64-v5.1.2.1/
 sudo ./install.sh
-echo "Prime sense drivers correctly installed"
-echo "Install NITE for skeleton traking"
+echo -e "${FRM}${RED}${BGYELLOW}Prime sense drivers correctly installed${NC}"
+echo -e "${FRM}${WHITE}${BGBLUE}Installing NITE for skeleton traking${NC}"
 cd $HOME
 nite_file="$(pwd)/NITE-Bin-Linux-x64-v1.5.2.23.tar.zip"
 if [ ! -f "$nite_file" ]; then
@@ -48,8 +66,8 @@ if [ ! -f "$nite_file" ]; then
 fi
 cd NITE-Bin-Dev-Linux-x64-v1.5.2.23
 sudo ./install.sh
-echo "NITE correctly installed"
-echo "Try to install OpenNI to update default lib"
+echo -e "${FRM}${RED}${BGYELLOW}NITE correctly installed ${NC}"
+echo -e "${FRM}${WHITE}${BGBLUE}Trying to install OpenNI to update default libraries${NC}"
 cd $HOME
 openni_file_dir="$(pwd)/OpenNI"
 if [ ! -f "$openni_file_dir" ]; then
@@ -61,7 +79,7 @@ cd Platform/Linux/CreateRedist
 ./RedistMaker	
 cd ../Redist/OpenNI-Bin-Dev-Linux-x64-v1.5.8.5/
 sudo ./install.sh
-echo "OpenNI correctly installed"
+echo -e "${FRM}${RED}${BGYELLOW} OpenNI correctly installed${NC}"
 sudo apt-get -y install ros-kinetic-urg-node
 sudo apt-get -y install ros-kinetic-joy
 sudo apt-get -y install ros-kinetic-openni-camera
@@ -73,12 +91,13 @@ sudo apt-get -y install ros-kinetic-tf2-bullet
 sudo apt-get -y install ros-kinetic-fake-localization
 sudo apt-get -y install ros-kinetic-map-server
 sudo apt-get -y install ros-kinetic-sound-play
-echo "INSTALLING OTHER NEEDED LIBRARIES..."
+echo -e "${FRM}${WHITE}${BGBLUE}Installing basic audio libraries${NC}"
 sudo apt-get -y install libzbar-dev
 sudo apt-get -y purge jackd2 jackd jackd2-firewire libjack-jackd2-dev pulseaudio-module-jack qjackctl
-echo "PLEASE SAY YES WHEN ASKED FOR REAL TIME"
+echo -e "${FRM}${WHITE}${BGBLUE}Audio support will be installed, choose <yes> when asked for real time permissions${NC}"
+read -p "(Waiting for key press in order to continue)"
 sudo apt-get -y install jackd2 libjack-jackd2-dev pulseaudio-module-jack qjackctl
-echo "INSTALLING pyaudio lib to the isra's node"
+echo -e "${FRM}${WHITE}${BGBLUE}Installing pyaudio lib for directional audio node${NC}"
 sudo apt-get -y install libasound-dev portaudio19-dev libportaudio2 libportaudiocpp0
 sudo apt-get -y install ffmpeg libav-tools
 sudo easy_install pip
@@ -107,4 +126,4 @@ for f in $FILES
                  #echo $f
                  #mv $f $f
         done
-echo "YOUR NEW JUSTINA-SOFTWARE IS READY TO USE :D"
+echo -e "${FRM}${RED}${BGYELLOW}You can now ${NC}${FRM}${PURPLE}${BGYELLOW}behold ${FRM}${RED}${BGYELLOW}${NC} the power of Justina software${NC}"
