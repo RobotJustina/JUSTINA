110d7fc8dadd83943cd9b8e6b6c6eda4db972477
Reynaldo Martell (Todavía más negra) <reymar_44@hotmail.com>
2017-06-22 12:26:23 -0500

    Add function test for find gestures

---
+++ catkin_ws/src/tests/func_test/src/find_gesture_test.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include "justina_tools/JustinaTasks.h"
+
+int main(int argc, char** argv)
+{
+    std::cout << "INITIALIZING A TEST JUST FOR TEST BY MARCOSOFT..." << std::endl;
+    ros::init(argc, argv, "act_pln");
+    ros::NodeHandle n;
+    JustinaTasks::setNodeHandle(&n);
+    ros::Rate loop(10);
+
+    int nextState = 1;
+    bool fail = false;
+    bool success = false;
+    std::string gesture = "pointing_right";
+    bool isFound;
+
+    while(ros::ok() && !fail && !success){
+        switch(nextState){
+        case 1:
+            std::cout << "trying find a person " << gesture << std::endl;
+            isFound = JustinaTasks::findGesturePerson(gesture);
+            if(!isFound){
+                std::cout << "Can not find a person " << gesture << std::endl;
+                nextState = 1;
+            }
+            else
+                nextState = 2;
+            break;
+        default:
+            std::cout << "NavigTest.->Somebody very stupid programmed this shit. " << std::endl;
+            fail = true;
+            break;
+        }
+        ros::spinOnce();
+        loop.sleep();
+    }
+
+    return 0;
+}
