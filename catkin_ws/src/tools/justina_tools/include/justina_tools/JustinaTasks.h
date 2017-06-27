c9318bb50e0a96413b26696da7704a8bb3f1a347
rmartella <reymar_44@hotmail.com>
2017-06-27 15:56:04 -0500

    Add the pose parameter in find person task

--- catkin_ws/src/tools/justina_tools/include/justina_tools/JustinaTasks.h
+++ catkin_ws/src/tools/justina_tools/include/justina_tools/JustinaTasks.h
@@ -34,6 +34,13 @@ public:
         SM_FOLLOWING_FINISHED
     };
 
+    enum POSE{
+        NONE,
+        STANDING,
+        SITTING,
+        LYING
+    };
+
     static bool setNodeHandle(ros::NodeHandle* nh);
     static bool alignWithTable();
     static bool alignWithTable(float distToTable);
@@ -43,9 +50,9 @@ public:
     static bool placeObject(bool withLeftArm, float h = 0, bool placeBag = false);
     static void sayAndAsyncNavigateToLoc(std::string location, bool say = true);
     static bool sayAndSyncNavigateToLoc(std::string location, int timeout, bool say = true);
-	static bool waitRecognizedFace(float timeout, std::string id, int gender, std::vector<vision_msgs::VisionFaceObject> &faces);
+	static bool waitRecognizedFace(float timeout, std::string id, int gender, POSE pose, std::vector<vision_msgs::VisionFaceObject> &faces);
 	static bool waitRecognizedGesture(std::vector<vision_msgs::GestureSkeleton> &gestures, float timeout);
-	static bool findPerson(std::string person = "", int gender = -1);
+	static bool findPerson(std::string person = "", int gender = -1, POSE pose = NONE);
     static bool findGesturePerson(std::string gesture);
     static bool tellGenderPerson(std::string &gender);
     static int manyGenderPerson(int gender);
@@ -61,7 +68,7 @@ public:
 
 private:
 	static bool getNearestRecognizedFace(std::vector<vision_msgs::VisionFaceObject> facesObject, float distanceMax, Eigen::Vector3d &centroidFace, int &genderRecog);
-	static bool turnAndRecognizeFace(std::string id, int gender, float initAngPan, float incAngPan,float maxAngPan, float initAngTil, float incAngTil, float maxAngTil,float incAngleTurn, float maxAngleTurn, Eigen::Vector3d &centroidFace, int &genderRecog);
+	static bool turnAndRecognizeFace(std::string id, int gender, POSE pose, float initAngPan, float incAngPan,float maxAngPan, float initAngTil, float incAngTil, float maxAngTil,float incAngleTurn, float maxAngleTurn, Eigen::Vector3d &centroidFace, int &genderRecog);
 	static bool getNearestRecognizedGesture(std::string typeGesture, std::vector<vision_msgs::GestureSkeleton> gestures, float distanceMax, Eigen::Vector3d &nearestGesture);
 	static bool turnAndRecognizeGesture(std::string typeGesture, float initAngPan, float incAngPan, float maxAngPan, float initAngTil, float incAngTil, float maxAngTil, float incAngleTurn, float maxAngleTurn, Eigen::Vector3d &gesturePos);
 };
