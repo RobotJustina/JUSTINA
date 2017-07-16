821cc5d6a41e3d6e6e79b7980a6923ec76da3d7f
rmartella <reymar_44@hotmail.com>
2017-07-15 22:30:12 -0500

    Checkout from TheAmazingNavigation mvn_pln and obs_detect

--- catkin_ws/src/navigation/mvn_pln/src/MvnPln.h
+++ catkin_ws/src/navigation/mvn_pln/src/MvnPln.h
@@ -82,11 +82,13 @@ private:
     float collisionPointX;
     float collisionPointY;
     bool stopReceived;
+    bool _allow_move_lateral;
     sensor_msgs::LaserScan lastLaserScan;
 
 public:
     void initROSConnection(ros::NodeHandle* nh);
     void spin();
+    void allow_move_lateral(bool _allow_move_lateral);
 
 private:
     bool planPath(float startX, float startY, float goalX, float goalY, nav_msgs::Path& path);
