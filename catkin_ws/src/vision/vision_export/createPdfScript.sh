bcbc3e38fb01b012839e49dd46ec286d7c93c5a6
Hector Rodrigo Arce Gonzalez <rag_@live.com>
2017-07-25 23:10:02 -0500

    scripts now do more magic

--- catkin_ws/src/vision/vision_export/createPdfScript.sh
+++ catkin_ws/src/vision/vision_export/createPdfScript.sh
@@ -5,6 +5,7 @@ TEST=$1
 if (($# < 1 )); then
     echo "No input parameters where supplied:";
     echo "1st parameter is test and file name";
+    echo "2nd argument (optional) is the path to clean before";
     exit 0
 fi
 #erase at the script start
@@ -13,6 +14,18 @@ rm /home/$USER/JUSTINA/catkin_ws/src/vision/vision_export/temp/*.log;
 rm /home/$USER/JUSTINA/catkin_ws/src/vision/vision_export/temp/*.aux;
 rm $OPATH/*.aux;
 rm $OPATH/*.log;
+#optional stuff
+if [ -z "$2" ]; then
+	echo "no dir to clean, PDF created normally"
+else
+	mkdir /home/$USER/JUSTINA/catkin_ws/src/vision/vision_export/temp/files_from_$USER-$(date +"%H-%M-%S")
+	for i in ${@:2}
+	do
+	    echo "directory $i will be cleaned"
+	    mv $i /home/$USER/JUSTINA/catkin_ws/src/vision/vision_export/temp/files_from_$USER-$(date +"%H-%M-%S")/
+	    mkdir $i
+	done
+fi
 #and his name will be...
 FILE=/home/$USER/JUSTINA/catkin_ws/src/vision/vision_export/temp/PUMAS_$TEST-$(date +"%Y-%m-%d").tex;
 echo "\\documentclass{article}" >> $FILE;
