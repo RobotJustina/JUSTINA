5c0ff9d4848948c2432c7a3a53ac2b7a08fbd658
edgarVazquez43 <edgarvazquez1403@gmail.com>
2017-08-18 19:09:47 -0500

    Add a marker for DirectKinematic

--- catkin_ws/src/test/grasp_pln/src/grasp_object_test.cpp
+++ catkin_ws/src/test/grasp_pln/src/grasp_object_test.cpp
@@ -1,36 +1,50 @@
 #include <iostream>
 #include "ros/ros.h"
-#include <visualization_msgs/Marker.h>
 #include <geometry_msgs/Vector3.h>
-#include "vision_msgs/DetectObjects.h"
-#include "manip_msgs/InverseKinematicsFloatArray.h"
-#include "manip_msgs/DirectKinematics.h"
 #include <tf/transform_listener.h>
+#include <visualization_msgs/Marker.h>
+#include "vision_msgs/DetectObjects.h"
+#include "manip_msgs/DirectKinematics.h"
+#include "manip_msgs/InverseKinematicsFloatArray.h"
 
+visualization_msgs::Marker endEffector_marker;
 visualization_msgs::Marker centroid_marker, axis_list_marker;
 
 bool markerSetup()
 {
 
     centroid_marker.header.frame_id = "base_link";
+    endEffector_marker.header.frame_id = "base_link";
     axis_list_marker.header.frame_id = "base_link";
+    
     centroid_marker.header.stamp = ros::Time::now();
+    endEffector_marker.header.stamp = ros::Time::now();
     axis_list_marker.header.stamp = ros::Time::now();
+
     centroid_marker.ns = "centroid";
     axis_list_marker.ns = "principal axis";
+    endEffector_marker.ns = "endEffector_r";
+    
     centroid_marker.pose.orientation.w = 1.0;
+    endEffector_marker.pose.orientation.w = 1.0;
     axis_list_marker.pose.orientation.w = 1.0;
 
     centroid_marker.id = 0;
+    endEffector_marker.id = 0;
     axis_list_marker.id = 1;
 
     centroid_marker.type = visualization_msgs::Marker::SPHERE;
+    endEffector_marker.type = visualization_msgs::Marker::SPHERE;
     axis_list_marker.type = visualization_msgs::Marker::LINE_LIST;
 
     // POINTS markers use x and y scale for width/height respectively
     centroid_marker.scale.x = 0.035;
     centroid_marker.scale.y = 0.035;
     centroid_marker.scale.z = 0.035;
+
+    endEffector_marker.scale.x = 0.040;
+    endEffector_marker.scale.y = 0.040;
+    endEffector_marker.scale.z = 0.040;
 
     axis_list_marker.scale.x = 0.03;
     axis_list_marker.scale.y = 0.03;
@@ -39,6 +53,9 @@ bool markerSetup()
     centroid_marker.color.b = 1.0f;
     centroid_marker.color.a = 1.0;
 
+    endEffector_marker.color.g = 1.0f;
+    endEffector_marker.color.a = 1.0;
+
     axis_list_marker.color.r = 1.0f;
     axis_list_marker.color.a = 1.0;
 
@@ -46,9 +63,9 @@ bool markerSetup()
 }
 
 visualization_msgs::Marker buildMarkerAxis(geometry_msgs::Vector3 PCA_axis_0,
-                                          geometry_msgs::Vector3 PCA_axis_1,
-                                          geometry_msgs::Vector3 PCA_axis_2,
-                                          geometry_msgs::Pose centroid_pose)
+					   geometry_msgs::Vector3 PCA_axis_1,
+					   geometry_msgs::Vector3 PCA_axis_2,
+					   geometry_msgs::Pose centroid_pose)
 {
     axis_list_marker.points.clear();
     geometry_msgs::Point px_centroid;
@@ -100,7 +117,7 @@ int main(int argc, char** argv)
     tf::StampedTransform transform;
 
 
-    geometry_msgs::Pose centroid;
+    geometry_msgs::Pose centroid, endEffector_pose;
     geometry_msgs::Vector3 axis_resp_0, axis_resp_1, axis_resp_2;
 
     ros::ServiceClient cltDetectObjectsPCA;
@@ -143,13 +160,12 @@ int main(int argc, char** argv)
 
     while(ros::ok())
     {
-
+      
         if(!cltDetectObjectsPCA.call(srv_detectObj))
         {
             std::cout << std::endl << "Justina::Vision can't detect anything" << std::endl << std::endl;
             return false;
         }
-
 
         /*
         if(!cltInverseKinematics.call(srv_ki))
@@ -168,7 +184,8 @@ int main(int argc, char** argv)
         else
         {
             std::cout << "DirectKinematics.-> Calculated cartesian...." << std::endl;
-            for (int i=0; i < 7; i++) std::cout << "   " << srv_kd.response.cartesian_pose.data[i] << std::endl;
+	    std::cout << "[x, y, z, roll, pitch, yaw]" << std::endl;
+	    for (int i=0; i < 7; i++) std::cout << "   " << srv_kd.response.cartesian_pose.data[i] << std::endl;
 
             listener.lookupTransform("/base_link", "/base_ra_arm", ros::Time(0), transform);
 
@@ -180,14 +197,14 @@ int main(int argc, char** argv)
             std::cout << "    x = " << v.x() << std::endl;
             std::cout << "    y = " << v.y() << std::endl;
             std::cout << "    z = " << v.z() << std::endl;
-            centroid.position.x = v.x();
-            centroid.position.y = v.y();
-            centroid.position.z = v.z();
+            endEffector_pose.position.x = v.x();
+            endEffector_pose.position.y = v.y();
+            endEffector_pose.position.z = v.z();
         }
 
 
 
-        //centroid = srv_detectObj.response.recog_objects[0].pose;
+        centroid = srv_detectObj.response.recog_objects[0].pose;
         axis_resp_0 = srv_detectObj.response.recog_objects[0].principal_axis[0];
         axis_resp_1 = srv_detectObj.response.recog_objects[0].principal_axis[1];
         axis_resp_2 = srv_detectObj.response.recog_objects[0].principal_axis[2];
@@ -199,15 +216,16 @@ int main(int argc, char** argv)
         //std::cout << srv.response.recog_objects[0].principal_axis[1] << std::endl;
         //std::cout << srv.response.recog_objects[0].principal_axis[2] << std::endl;
 
+	endEffector_marker.pose.position = endEffector_pose.position;
         centroid_marker.pose.position = centroid.position;
         //centroid_marker.pose.position.x = v.x();
         //centroid_marker.pose.position.y = v.y();
         //centroid_marker.pose.position.z = v.z();
-        marker_pub.publish(centroid_marker);
+        marker_pub.publish(endEffector_marker);
         axis_list_marker = buildMarkerAxis(axis_resp_0,
-                    axis_resp_1,
-                    axis_resp_2,
-                    centroid);
+					   axis_resp_1,
+					   axis_resp_2,
+					   centroid);
         marker_pub.publish(axis_list_marker);
 
         std::cout << "---------------------------" << std::endl;
