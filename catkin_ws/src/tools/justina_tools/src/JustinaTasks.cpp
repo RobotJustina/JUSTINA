a8cc27426427a5dc46ba417a33df5869f7f74020
rmartella <reymar_44@hotmail.com>
2017-11-23 21:16:09 -0600

    Add the maxz componente to grasp from the top of cube

--- catkin_ws/src/tools/justina_tools/src/JustinaTasks.cpp
+++ catkin_ws/src/tools/justina_tools/src/JustinaTasks.cpp
@@ -3054,7 +3054,9 @@ bool JustinaTasks::graspBlockFeedback(float x, float y, float z, bool withLeftAr
             << std::endl;
         objToGraspX = cubes.recog_cubes.at(0).cube_centroid.x;
         objToGraspY = cubes.recog_cubes.at(0).cube_centroid.y;
-        objToGraspZ = (cubes.recog_cubes.at(0).cube_centroid.z + cubes.recog_cubes.at(0).maxPoint.z) / 2.0f;
+        //objToGraspZ = (cubes.recog_cubes.at(0).cube_centroid.z + cubes.recog_cubes.at(0).maxPoint.z) / 2.0f;
+        //objToGraspZ = cubes.recog_cubes.at(0).cube_centroid.z;
+        objToGraspZ = cubes.recog_cubes.at(0).maxPoint.z;
     } else if (!found && idBlock.compare("") == 0) {
         std::cout
             << "The object was not found again, update new coordinates with the motion of robot."
