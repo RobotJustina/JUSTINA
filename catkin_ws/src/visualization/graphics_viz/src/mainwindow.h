d062475130b3072c2425063fa939a921cf985c0d
rmartella <reymar_44@hotmail.com>
2017-11-27 00:45:32 -0600

    Add the layout for the viz gui to create a world model

--- catkin_ws/src/visualization/graphics_viz/src/mainwindow.h
+++ catkin_ws/src/visualization/graphics_viz/src/mainwindow.h
@@ -3,6 +3,7 @@
 
 #include "QtRosNode.h"
 #include <QMainWindow>
+#include <QMenu>
 
 namespace Ui {
 class MainWindow;
@@ -16,7 +17,12 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
     void setQtRosNode(QtRosNode *qtRosNode);
+
 private slots:
+    void on_delete_2_pressed();
+    void on_add_pressed();
+    void on_copy_pressed();
+    void on_rename_pressed();
     void updateGraphicsSlot();
     void closeEvent(QCloseEvent *event);
 private:
