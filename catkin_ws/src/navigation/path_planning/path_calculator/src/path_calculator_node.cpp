171a2ed6f91926c3dd0e012d773a99aef0659056
rmartella <reymar_44@hotmail.com>
2018-03-30 14:06:17 -0600

    Add the publish of the path grown map

--- catkin_ws/src/navigation/path_planning/path_calculator/src/path_calculator_node.cpp
+++ catkin_ws/src/navigation/path_planning/path_calculator/src/path_calculator_node.cpp
@@ -9,6 +9,8 @@
 #include "tf/tf.h"
 #include "PathCalculator.h"
 
+ros::Publisher pubMapGrown;
+
 bool callbackWaveFrontFromMap(navig_msgs::PathFromMap::Request &req, navig_msgs::PathFromMap::Response &resp)
 {
     return PathCalculator::WaveFront(req.map, req.start_pose, req.goal_pose, resp.path);
@@ -16,7 +18,9 @@ bool callbackWaveFrontFromMap(navig_msgs::PathFromMap::Request &req, navig_msgs:
 
 bool callbackAStarFromMap(navig_msgs::PathFromMap::Request &req, navig_msgs::PathFromMap::Response &resp)
 {
+    std::cout << "Reciving path calculatro request ------------------------------" << std::endl;
     bool success = PathCalculator::AStar(req.map, req.start_pose, req.goal_pose, resp.path);
+    pubMapGrown.publish(req.map);
     if(success)
     {
         resp.path = PathCalculator::SmoothPath(resp.path);
@@ -31,6 +35,7 @@ int main(int argc, char** argv)
     ros::NodeHandle n;
     ros::ServiceServer srvPathWaveFrontFromMap = n.advertiseService("path_calculator/wave_front_from_map", callbackWaveFrontFromMap);
     ros::ServiceServer srvPathAStarFromMap = n.advertiseService("path_calculator/a_star_from_map", callbackAStarFromMap);
+    pubMapGrown = n.advertise<nav_msgs::OccupancyGrid>("path_calculator/grown_map", 1); 
     ros::Rate loop(10);
 
     while(ros::ok())
