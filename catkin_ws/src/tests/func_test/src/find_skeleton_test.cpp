052e30eb93a038e9e909a1e103b063920d73d1b5
JulioCCruzE <jc.shogun.t6s@gmail.com>
2018-06-10 13:36:25 -0500

    fix parm of find skeleton function

--- catkin_ws/src/tests/func_test/src/find_skeleton_test.cpp
+++ catkin_ws/src/tests/func_test/src/find_skeleton_test.cpp
@@ -19,7 +19,7 @@ int main(int argc, char** argv)
         switch(nextState){
         case 1:
             std::cout << "trying find a person " << person << std::endl;
-            isFound = JustinaTasks::findSkeletonPerson("");
+            isFound = JustinaTasks::findSkeletonPerson(JustinaTasks::NONE, "");
             if(!isFound){
                 std::cout << "Can not find a person " << person << std::endl;
                 nextState = 1;
