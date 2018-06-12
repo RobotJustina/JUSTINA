9e40449a6da7f074ecbb12855cdaeafba74e3ecd
rmartella <reymar_44@hotmail.com>
2018-06-11 19:03:18 -0500

    Change the order of open pose starting

--- catkin_ws/src/tools/justina_tools/src/JustinaTasks.cpp
+++ catkin_ws/src/tools/justina_tools/src/JustinaTasks.cpp
@@ -1336,21 +1336,27 @@ bool JustinaTasks::findSkeletonPerson(POSE pose, std::string location){
 	std::stringstream ss;
 	std::string gestureSpeech;
 
-	JustinaVision::startSkeletonFinding();
-
-	JustinaManip::startHdGoTo(0, 0.0);
-	JustinaManip::waitForHdGoalReached(5000);
-
 	ss << "I am going to find you";
 	JustinaHRI::insertAsyncSpeech(ss.str(), 500);
 	JustinaHRI::asyncSpeech();
 	//JustinaHRI::waitAfterSay(ss.str(), 2000);
-
+	
+    JustinaVision::startSkeletonFinding();
+    boost::this_thread::sleep(boost::posix_time::milliseconds(300));
+    ros::spinOnce();
+    boost::this_thread::sleep(boost::posix_time::milliseconds(300));
+    
+    JustinaManip::startHdGoTo(0, 0.0);
+	JustinaManip::waitForHdGoalReached(5000);
+    	
 	Eigen::Vector3d centroid;
 	bool recog = JustinaTasks::turnAndRecognizeSkeleton(pose, -M_PI_4, M_PI_4 / 2.0, M_PI_4, -0.3, -0.2, -0.5, M_PI_2, 2 * M_PI, 3.0, centroid, location);
 	std::cout << "Centroid Gesture in coordinates of robot:" << centroid(0, 0) << "," << centroid(1, 0) << "," << centroid(2, 0) << ")";
 	std::cout << std::endl;
 	JustinaVision::stopSkeletonFinding();
+    boost::this_thread::sleep(boost::posix_time::milliseconds(300));
+    ros::spinOnce();
+    boost::this_thread::sleep(boost::posix_time::milliseconds(300));
 
 	ss.str("");
 	if (!recog) {
@@ -1390,11 +1396,6 @@ bool JustinaTasks::findGesturePerson(std::string gesture, std::string location){
 	std::stringstream ss;
 	std::string gestureSpeech;
 
-	JustinaVision::startSkeletonFinding();
-
-	JustinaManip::startHdGoTo(0, 0.0);
-	JustinaManip::waitForHdGoalReached(5000);
-
 	if(gesture.compare("pointing_left") == 0)
 		gestureSpeech = "pointing left";
 	if(gesture.compare("pointing_right") == 0)
@@ -1412,12 +1413,23 @@ bool JustinaTasks::findGesturePerson(std::string gesture, std::string location){
 	JustinaHRI::insertAsyncSpeech(ss.str(), 500);
 	JustinaHRI::asyncSpeech();
 	//JustinaHRI::waitAfterSay(ss.str(), 2000);
+    
+	JustinaVision::startSkeletonFinding();
+    boost::this_thread::sleep(boost::posix_time::milliseconds(300));
+    ros::spinOnce();
+    boost::this_thread::sleep(boost::posix_time::milliseconds(300));
+
+	JustinaManip::startHdGoTo(0, 0.0);
+	JustinaManip::waitForHdGoalReached(5000);
 
 	Eigen::Vector3d centroidGesture;
 	bool recog = JustinaTasks::turnAndRecognizeGesture(gesture, -M_PI_4, M_PI_4 / 2.0, M_PI_4, -0.3, -0.2, -0.5, M_PI_2, 2 * M_PI, 3.0, centroidGesture, location, false);
 	std::cout << "Centroid Gesture in coordinates of robot:" << centroidGesture(0, 0) << "," << centroidGesture(1, 0) << "," << centroidGesture(2, 0) << ")";
 	std::cout << std::endl;
 	JustinaVision::stopSkeletonFinding();
+    boost::this_thread::sleep(boost::posix_time::milliseconds(300));
+    ros::spinOnce();
+    boost::this_thread::sleep(boost::posix_time::milliseconds(300));
 
 	ss.str("");
 	if (!recog) {
