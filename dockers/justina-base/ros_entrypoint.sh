b0302cc92b3a84bf282d0e9f6e3a3cc7519d8b51
rmartella <reymar_44@hotmail.com>
2018-08-09 14:04:16 -0500

    Change the docker file config

--- dockers/justina-base/ros_entrypoint.sh
+++ dockers/justina-base/ros_entrypoint.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 set -e
+
 # setup ros environment
 source "/opt/ros/$ROS_DISTRO/setup.bash"
+echo "source \"/opt/ros/$ROS_DISTRO/setup.bash\"" >> $HOME/.bashrc
+echo "export ROS_MASTER_URI=http://$HOST_NAME:11311/" >> $HOME/.bashrc
+source ~/.bashrc
+echo 'biorobotica' | sudo -S -- sh -c "echo \"172.17.0.1   $HOST_NAME\" >> /etc/hosts"
+echo -e "\n"
+#echo 'test' | sudo tee -a /etc/hosts
 exec "$@"
