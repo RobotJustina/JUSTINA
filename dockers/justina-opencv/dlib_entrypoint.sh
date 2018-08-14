74191985f0bb2a54e9557e4ce905427592a8a835
rmartella <reymar_44@hotmail.com>
2018-08-14 18:39:00 -0500

    Add the python path to dlib entry point

--- dockers/justina-opencv/dlib_entrypoint.sh
+++ dockers/justina-opencv/dlib_entrypoint.sh
@@ -3,4 +3,5 @@ set -e
 
 #setup dlib python environment
 echo "export PYTHONPATH=$PYTHONPATH:$HOME/docker_volumen/facenet" >> $HOME/.bashrc
+echo "export PYTHONPATH=$PYTHONPATH:$HOME/docker_volumen/facenet/src" >> $HOME/.bashrc
 exec "$@"
