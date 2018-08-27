56039672a1aab004252a4552fc42ec2d22c87f27
rmartella <reymar_44@hotmail.com>
2018-08-26 22:35:49 -0500

    Add correct the PYTHONPATH

--- dockers/justina-dev/ros_workspace_entrypoint.sh
+++ dockers/justina-dev/ros_workspace_entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+#setup facenet python environment
+echo "export PYTHONPATH=$PYTHONPATH:$HOME/docker_volumen/facenet" >> $HOME/.bashrc
+echo "export PYTHONPATH=$PYTHONPATH:$HOME/docker_volumen/facenet/src" >> $HOME/.bashrc
 # setup workspace environment
 if [ ! -z ${ROS_WORKSPACE+x} ]; then
 	echo "source \"${ROS_WORKSPACE}/devel/setup.bash\"" >> $HOME/.bashrc
