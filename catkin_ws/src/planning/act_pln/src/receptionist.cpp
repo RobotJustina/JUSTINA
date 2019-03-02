d0adc458407e5372a92baa6db7c7a4634ef10317
rmartella <reymar_44@hotmail.com>
2019-03-02 09:49:29 -0600

    Fix bugs in receptcionist Cmkae list

--- catkin_ws/src/planning/act_pln/src/receptionist.cpp
+++ catkin_ws/src/planning/act_pln/src/receptionist.cpp
@@ -14,24 +14,19 @@
 #include "std_msgs/Bool.h"
 #include "string"
 
-enum STATES{
+enum STATE{
     SM_INIT,
     SM_WAIT_FOR_PERSON_ENTRANCE,
     
 };
 
 
-#define SM_INIT 0
-#define SM_WAIT_FOR_INIT_COMMAND 10
-#define SM_SEARCH_WAVING 20
-#define SM_CLOSE_TO_GUEST 30
-#define SM_RecognizeGuest 40
-#define SM_ReturnSearchWaving 50
-#define SM_GoCoatRack 60
-#define SM_SearchTaxiDriver 70
-#define SM_CLOSE_TO_TAXI_DRIVER 80
-
 int main(int argc, char **argv){
 
+    STATE state = SM_INIT;
 
+    switch(state){
+    }
+
+    return 1;
 }
