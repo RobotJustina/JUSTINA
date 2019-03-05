88f74dfafea93008f113eaaf0af72ed04a17d1e7
rmartella <reymar_44@hotmail.com>
2019-03-04 22:48:34 -0600

    Correct some bugs in trajectory by luis

--- catkin_ws/src/manipulation/trajectory_gen/src/first_trajectory.cpp
+++ catkin_ws/src/manipulation/trajectory_gen/src/first_trajectory.cpp
@@ -7,8 +7,8 @@ using namespace ros;
 
 string port;
 int baudRate;
-bool bulkEnable = true, syncWriteEnable = true;
-uint16_t goalPos[7] = {1543, 1600, 1800, 2100, 2048, 1800, 1050};
+bool bulkEnable = true, syncWriteEnable = false;
+uint16_t goalPos[7] = {1543, 1694, 1742, 2100, 2048, 2066, 1050};
 int goalSpeeds[7] = {};
 uint16_t curr_position[7] = {};
 
@@ -25,23 +25,44 @@ int main(int argc, char **argv)
     showDates();
 
     vector<int> ids;
-    for(int i = 0; i<9; i++){
+    for(int i = 0; i<7; i++){
         ids.push_back(i);
     }
     for(int i=0; i<7; i++)
         goalSpeeds[i] = 40;
 
     DynamixelManager dynamixelManager;
-    dynamixelManager.init(port, baudRate, bulkEnable, ids, syncWriteEnable);
+    dynamixelManager.enableInfoLevelDebug();
+    dynamixelManager.init(port, baudRate, bulkEnable, ids, false);
+
+    for(int i = 0; i < 7; i++){
+        dynamixelManager.enableTorque(i);
+        dynamixelManager.setPGain(i, 32);
+        dynamixelManager.setIGain(i, 0);
+        dynamixelManager.setDGain(i, 128);
+        /*dynamixelManager.setPGain(i, 32);
+          dynamixelManager.setIGain(i, 0);
+          dynamixelManager.setDGain(i, 0);*/
+        dynamixelManager.setMaxTorque(i, 1023);
+        dynamixelManager.setTorqueLimit(i, 768);
+        dynamixelManager.setHighestLimitTemperature(i, 80);
+        dynamixelManager.setAlarmShutdown(i, 0b00000100);
+    }
 
     for(int i=0; i < 7; i++)
         std::cout << "Pos" << goalPos[i] << std::endl;
     std::cout << "Setting all goal positions" << std::endl;
     //Setting the zero position of left arm
+    dynamixelManager.readBulkData();
+    for(int i = 0; i < 9; i++)
+        dynamixelManager.getPresentPosition(i, curr_position[i]);
     for (int i = 0; i < 7; i++){
-        dynamixelManager.setGoalPosition(i, goalPos[i]);		
+        std::cout << "index: " << i << std::endl;
+        std::cout << "Pos" << goalPos[i] << std::endl;
         dynamixelManager.setMovingSpeed(i, 30);
         std::cout << "index: " << i << std::endl;
+        std::cout << "Pos" << goalPos[i] << std::endl;
+        dynamixelManager.setGoalPosition(i, goalPos[i]);		
     }
     std::cout << "Sending write command..." << std::endl;
     if(syncWriteEnable){
@@ -52,8 +73,8 @@ int main(int argc, char **argv)
     Duration d = Duration(1);
     d.sleep();	
     //Setting the new position of left shoulder
-    dynamixelManager.setGoalPosition(0, 1300);
     dynamixelManager.setMovingSpeed(0,30);
+    dynamixelManager.setGoalPosition(0, 1300);
     if(syncWriteEnable){
         dynamixelManager.writeSyncGoalPosesData();
         dynamixelManager.writeSyncSpeedsData();
