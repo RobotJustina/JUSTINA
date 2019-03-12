1ddde32a638ef68b744b8422b7012f72a49d993e
Luis Nava <levacarrillo@gmail.com>
2019-03-12 14:13:47 -0600

    New service for making profiles fixed reach_positions node

--- catkin_ws/src/manipulation/trajectory_gen/src/reach_positions.cpp
+++ catkin_ws/src/manipulation/trajectory_gen/src/reach_positions.cpp
@@ -2,40 +2,59 @@
 #include<std_msgs/Float32MultiArray.h>
 #include<vector>
 
-#define OBJETIVE_POSE    1.7    // 1.7 rad
-#define CONSTANT_VELOCITY 0.13   // 13% of maximum velocity 
+#define ID_O        0
+#define ID_1        1
+#define ID_2        2
+#define ID_3        3
+#define ID_4        4
+#define ID_5        5
+#define ID_6        6
+
+#define CONSTANT_VELOCITY 0.08   // 13% of maximum velocity 
 #define DYNAMIXEL_MAX_VEL 12.259 // 12.259 rad/s  
 
 using namespace std;
 using namespace ros;
 
 int record = 0;
-float current_time = 0;
+bool connection = false;
 float goalPos[7] = {0, 0, 0, 0, 0, 0, 0};
-
-
 float constant_velocity = CONSTANT_VELOCITY * DYNAMIXEL_MAX_VEL; 
-float total_time = abs(OBJETIVE_POSE/constant_velocity);
 
-int num_velocities = total_time / 0.02;
-float* goalSpeeds = new float[num_velocities];
-float speeds_record();
+float time_for_id_[7]  = {0,0,0,0,0,0,0};
+
+
+int num_vel_for_id_[7] = {0,0,0,0,0,0,0};
+
+
+float goalSpeeds[7][10000];
+
+
+
+
+void speeds_record();
 
 
 void posesCallback(const std_msgs::Float32MultiArray::ConstPtr &msg){
-        cout<<"Reciving new goal poses..."<<endl;
-        //ROS_INFO("I heard: [%f]", msg->data[3]);
         if(!(msg->data.size() == 7 || msg->data.size() == 14))
-            cout << "Can not process the goal poses for the left arm" << endl;
+                cout << "Can not process the goal poses for the left arm" << endl;
         else{
-            goalPos[0] =  msg->data[0];
-            goalPos[1] =  msg->data[1];
-            goalPos[2] =  msg->data[2];
-            goalPos[3] =  msg->data[3];
-            goalPos[4] =  msg->data[4];
-            goalPos[5] =  msg->data[5];
-            goalPos[6] =  msg->data[6];                    
+                connection = true;
+            
+                goalPos[0] =  msg->data[0];
+                goalPos[1] =  msg->data[1];
+                goalPos[2] =  msg->data[2];
+                goalPos[3] =  msg->data[3];
+                goalPos[4] =  msg->data[4];
+                goalPos[5] =  msg->data[5];
+                goalPos[6] =  msg->data[6];    
+            
+                for(int i=0; i < 7 ; i++){
+                    time_for_id_[i] = abs( goalPos[i]  /  constant_velocity);                
+                    num_vel_for_id_[i] = time_for_id_[i] / 0.02;
+                }
         }
+
 }
 
 int main(int argc, char **argv){
@@ -47,40 +66,78 @@ int main(int argc, char **argv){
     Publisher pubGoalPos = node.advertise<std_msgs::Float32MultiArray>("/hardware/left_arm/goal_pose", 1000);
 
 
-
-
-    std_msgs::Float32MultiArray msg;
     Rate loop_rate(50);
+    std_msgs::Float32MultiArray msg;
+
+
+    while(connection == false){
+        cout<<"Waiting for goal poses..."<<endl;        
+        spinOnce();
+
+        loop_rate.sleep();
+    }
+    
 
     speeds_record();
+    Duration(2).sleep();
+
 
     while(ok()){
-    	
         msg.data.clear();
         //Setting positions for dynamixels
-        for(int i=0; i<7; i++){
+        for(int i=0; i<7; i++)
             msg.data.push_back(goalPos[i]);          
-        }
+
         //Seting velocities for dynamixels
         for(int i=7; i<14; i++){
-            msg.data.push_back(goalSpeeds[record]);
+            if(i == 7)
+                msg.data.push_back(goalSpeeds[ID_O][record]);
+            else if(i == 8)
+                msg.data.push_back(goalSpeeds[ID_1][record]);
+            else if(i == 9)
+                msg.data.push_back(goalSpeeds[ID_2][record]);
+            else if(i == 10)
+                msg.data.push_back(goalSpeeds[ID_3][record]);
+            else if(i == 11)
+                msg.data.push_back(goalSpeeds[ID_4][record]);
+            else if(i == 12)
+                msg.data.push_back(goalSpeeds[ID_5][record]);
+            else if(i == 13)
+                msg.data.push_back(goalSpeeds[ID_6][record]);
+            else
+                msg.data.push_back(0);
         }
-        cout<<"Time: "<<current_time<<"\tPosition: "<<goalPos[3]<<"\tvelocity: "<<goalSpeeds[record]<<endl;
 
-        current_time += 0.02;
         record++;
 
     	pubGoalPos.publish(msg);
+
     	spinOnce();
         loop_rate.sleep();
-        if(current_time > total_time)
-            return 0;
-    }//*/
+/*        if(record > num_vel_for_id_[ID_O] && record > num_vel_for_id_[ID_1] && record > num_vel_for_id_[ID_2] &&
+            record > num_vel_for_id_[ID_3] && record > num_vel_for_id_[ID_4] && record > num_vel_for_id_[ID_5] &&
+            record > num_vel_for_id_[ID_6])
+            return 0;//*/
+    }
 }//From int main()
 
 
 
-float speeds_record(){
-    for(int i=0 ; i <= num_velocities ; i++)
-        goalSpeeds[i] = (OBJETIVE_POSE * (30*pow(0.02*i, 2)/pow(total_time,3)  -  60*pow(0.02*i,3)/pow(total_time,4)  +  30*pow(0.02*i,4)/pow(total_time,5)))/DYNAMIXEL_MAX_VEL;//*/
+void speeds_record(){
+    for(int i=0 ; i <= num_vel_for_id_[ID_O] ; i++){
+        goalSpeeds[ID_O][i] = abs(goalPos[ID_O] * (30*pow(0.02*i, 2)/pow(time_for_id_[ID_O],3)  -  60*pow(0.02*i,3)/pow(time_for_id_[ID_O],4) 
+                                                         +  30*pow(0.02*i,4)/pow(time_for_id_[ID_O],5)))/DYNAMIXEL_MAX_VEL;//*/
+        goalSpeeds[ID_1][i] = abs(goalPos[ID_1] * (30*pow(0.02*i, 2)/pow(time_for_id_[ID_1],3)  -  60*pow(0.02*i,3)/pow(time_for_id_[ID_1],4) 
+                                                         +  30*pow(0.02*i,4)/pow(time_for_id_[ID_1],5)))/DYNAMIXEL_MAX_VEL;//*/
+        goalSpeeds[ID_2][i] = abs(goalPos[ID_2] * (30*pow(0.02*i, 2)/pow(time_for_id_[ID_2],3)  -  60*pow(0.02*i,3)/pow(time_for_id_[ID_2],4) 
+                                                         +  30*pow(0.02*i,4)/pow(time_for_id_[ID_2],5)))/DYNAMIXEL_MAX_VEL;//*/
+        goalSpeeds[ID_3][i] = abs(goalPos[ID_3] * (30*pow(0.02*i, 2)/pow(time_for_id_[ID_3],3)  -  60*pow(0.02*i,3)/pow(time_for_id_[ID_3],4) 
+                                                         +  30*pow(0.02*i,4)/pow(time_for_id_[ID_3],5)))/DYNAMIXEL_MAX_VEL;//*/
+        goalSpeeds[ID_4][i] = abs(goalPos[ID_4] * (30*pow(0.02*i, 2)/pow(time_for_id_[ID_4],3)  -  60*pow(0.02*i,3)/pow(time_for_id_[ID_4],4) 
+                                                         +  30*pow(0.02*i,4)/pow(time_for_id_[ID_4],5)))/DYNAMIXEL_MAX_VEL;//*/
+        goalSpeeds[ID_5][i] = abs(goalPos[ID_5] * (30*pow(0.02*i, 2)/pow(time_for_id_[ID_5],3)  -  60*pow(0.02*i,3)/pow(time_for_id_[ID_5],4) 
+                                                         +  30*pow(0.02*i,4)/pow(time_for_id_[ID_5],5)))/DYNAMIXEL_MAX_VEL;//*/ 
+        goalSpeeds[ID_6][i] = abs(goalPos[ID_6] * (30*pow(0.02*i, 2)/pow(time_for_id_[ID_6],3)  -  60*pow(0.02*i,3)/pow(time_for_id_[ID_6],4) 
+                                                         +  30*pow(0.02*i,4)/pow(time_for_id_[ID_6],5)))/DYNAMIXEL_MAX_VEL;//*/                                                                   
+    }//*/
 }
