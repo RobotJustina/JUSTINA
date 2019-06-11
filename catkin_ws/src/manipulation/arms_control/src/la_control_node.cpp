40550c4e0e31ff5145d7e3f26b1d99126c35b3f4
rmartella <reymar_44@hotmail.com>
2019-06-11 16:13:38 -0500

    Rules to run the low latency to arms

--- catkin_ws/src/manipulation/arms_control/src/la_control_node.cpp
+++ catkin_ws/src/manipulation/arms_control/src/la_control_node.cpp
@@ -93,9 +93,9 @@ int main(int argc, char** argv)
     std_msgs::Float32MultiArray      msg_la_goal_pose   ;
     std_msgs::Bool                   msg_la_goal_reached;
 
-    system("echo 1 | sudo tee /sys/bus/usb-serial/devices/ttyUSB0/latency_timer"); 
+    /*system("echo 1 | sudo tee /sys/bus/usb-serial/devices/ttyUSB0/latency_timer"); 
     system("echo 1 | sudo tee /sys/bus/usb-serial/devices/ttyUSB1/latency_timer");
-    system("echo 1 | sudo tee /sys/bus/usb-serial/devices/ttyUSB2/latency_timer");
+    system("echo 1 | sudo tee /sys/bus/usb-serial/devices/ttyUSB2/latency_timer");*/
 
     while(ros::ok())
     {
