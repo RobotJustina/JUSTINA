63a54d499b04b34acc102387f91f41fce7b28ba2
JulioCCruzE <jc.shogun.t6s@gmail.com>
2019-10-07 17:27:49 -0500

    add functions for robot and location pose

--- catkin_ws/src/planning/supervisor/src/supervisor_node.cpp
+++ catkin_ws/src/planning/supervisor/src/supervisor_node.cpp
@@ -2,7 +2,6 @@
 #include <sstream>
 
 #include "ros/ros.h"
-
 #include "knowledge_msgs/PlanningCmdClips.h"
 
 //#include "knowledge_msgs/kdbFilePath.h"
@@ -11,6 +10,18 @@
 //#include "std_msgs/Empty.h"
 //#include "std_msgs/String.h"
 //#include "std_msgs/ColorRGBA.h"
+
+#include "justina_tools/JustinaNavigation.h"
+#include "justina_tools/JustinaKnowledge.h"
+#include <boost/algorithm/string.hpp>
+#include <boost/algorithm/string/split.hpp>
+#include <boost/algorithm/string/classification.hpp>
+
+using namespace boost::algorithm;
+
+bool fnav = false;
+float x, y, theta;
+float sx, sy, st;
 
 ros::Publisher robot_pose_pub;
 
@@ -21,10 +32,29 @@ void callbackCmdNavigation(
 	std::cout << "name:" << msg->name << std::endl;
 	std::cout << "params:" << msg->params << std::endl;
 
-	std_msgs::Bool rp;
+	if(msg->name == "goto"){
+    		std::map<std::string, std::vector<float> > locations;
+        std::vector<std::string> tokens;
+        std::string str = msg->params;
+        split(tokens, str, is_any_of(" "));
+
+		std::cout << "Location: " << tokens[1] << std::endl;
+
+    		JustinaKnowledge::getKnownLocations(locations);
+		if(locations.find(tokens[1]) == locations.end())
+		{
+			std::cout << "MvnPln.->Cannot get close to \"" << tokens[1] << "\". It is not a known location. " << std::endl;
+		}
+		sx = locations.find(tokens[1])->second[0];
+		sy = locations.find(tokens[1])->second[1];
+
+		std::cout << "Location: " << sx << ", " << sy << std::endl;
+	}
+
+	/*std_msgs::Bool rp;
 	rp.data = 1;
 
-	robot_pose_pub.publish(rp);
+	robot_pose_pub.publish(rp);*/
 }
 
 void callbackCmdResponse(const knowledge_msgs::PlanningCmdClips::ConstPtr& msg){
@@ -33,11 +63,20 @@ void callbackCmdResponse(const knowledge_msgs::PlanningCmdClips::ConstPtr& msg){
 	std::cout << "name:" << msg->name << std::endl;
 	std::cout << "params:" << msg->params << std::endl;
 	std::cout << "successful:" << msg->successful << std::endl;
+
+
+	if(msg->name == "goto"){
+		JustinaNavigation::getRobotPose(x, y, theta);
+		std::cout << "Robot pose: " << x << ", " << y << ", " << theta << std::endl;
+		JustinaNavigation::getRobotPoseFromOdom(x, y, theta);
+		std::cout << "Robot pose from Odom: " << x << ", " << y << ", " << theta << std::endl;
+		std::cout << "Location: " << sx << ", " << sy << std::endl;
+	}
 	
-	std_msgs::Bool rp;
+	/*std_msgs::Bool rp;
 	rp.data = 0;
 
-	robot_pose_pub.publish(rp);
+	robot_pose_pub.publish(rp);*/
 }
 
 int main(int argc, char ** argv) {
@@ -50,6 +89,9 @@ int main(int argc, char ** argv) {
 	
 	ros::Subscriber subCmdResponse = nh.subscribe("/planning_clips/command_response", 1, callbackCmdResponse);
 	robot_pose_pub = nh.advertise<std_msgs::Bool>("/supervisor/robot_pose", 1);
+	
+	JustinaNavigation::setNodeHandle(&nh);
+    	JustinaKnowledge::setNodeHandle(&nh);
     
 
 	while (ros::ok()) {
