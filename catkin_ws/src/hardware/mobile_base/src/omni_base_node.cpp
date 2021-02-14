054754a12c3ae80cb455f1c1005980021d2a75ef
Luis Nava <levacarrillo@gmail.com>
2021-02-14 11:06:30 -0600

    commit for maerge omni base and justina hardware

--- catkin_ws/src/hardware/mobile_base/src/omni_base_node.cpp
+++ catkin_ws/src/hardware/mobile_base/src/omni_base_node.cpp
@@ -10,9 +10,12 @@
 #include <exception>
 #include <algorithm>
 
-#define BASE_WIDTH 0.52
-#define TICKS_PER_METER_FRONTAL	158891.2
-#define TICKS_PER_METER_LATERAL  164352.1
+#define BASE_WIDTH 0.502 //0.52
+//#define TICKS_PER_METER_FRONTAL 158891.2
+//#define TICKS_PER_METER_LATERAL 164352.1
+
+#define TICKS_PER_METER_FRONTAL  365063.24 //420920.958 //367735.5 
+#define TICKS_PER_METER_LATERAL  365063.24 //413641.555 //372277.4  
 
 bool newData = false;
 int noNewDataCounter = 5;
@@ -222,10 +225,10 @@ void callbackCmdVel(const geometry_msgs::Twist::ConstPtr &msg)
     {
         checkSpeedRanges(leftSpeed, rightSpeed, frontSpeed, rearSpeed);
 
-        leftSpeed   = (leftSpeed    * QPPS_LEFT * 16.0 / 35.0);
-        rightSpeed  = (rightSpeed   * QPPS_RIGHT * 16.0 / 35.0);
-        frontSpeed  = -(frontSpeed   * QPPS_FRONT * 16.0 / 35.0);
-        rearSpeed   = -(rearSpeed    * QPPS_REAR * 16.0 / 35.0);
+        leftSpeed   =  (leftSpeed     * QPPS_LEFT  );// * 16.0 / 35.0);
+        rightSpeed  =  (rightSpeed    * QPPS_RIGHT );// * 16.0 / 35.0);
+        frontSpeed  = -(frontSpeed    * QPPS_FRONT );// * 16.0 / 35.0);
+        rearSpeed   = -(rearSpeed     * QPPS_REAR  );// * 16.0 / 35.0);
 
         try
         {
